20afd4db7528de823914e03e3da2ebabc060c7ed
Aline <chetta.aline@gmail.com>
2020-07-16 11:05:25 +0200

    Add component Paragraph + Markdown

--- src/components/visualize/Block/Block.vue
+++ src/components/visualize/Block/Block.vue
@@ -9,10 +9,12 @@
 
 <script>
 import TableView from '../TableView/TableView.vue'
+import Markdown from '@/components/visualize/Markdown/Markdown'
+import Paragraph from '@/components/visualize/Paragrah/Paragraph'
 
 export default {
   name: 'Block',
-  components: { TableView },
+  components: { TableView, Paragraph, Markdown },
   props: {
     block: {
       type: Object,
