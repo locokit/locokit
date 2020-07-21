1ccbfa51573ea016c8ff25d9c1aa175340485973
Aline <chetta.aline@gmail.com>
2020-07-21 17:35:06 +0200

    Fixes #24: Manage invalid token

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import WorkspaceList from '../views/WorkspaceList.vue'
 import Profile from '../views/Profile.vue'
 import Page from '@/views/Page.vue'
 import { ROUTES_PATH } from './paths'
+import { authState } from '@/store/auth'
 
 Vue.use(VueRouter)
 
@@ -54,6 +55,28 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach(async function checkAuthentication (to, from, next) {
+  const needAuthentication = to.matched.some(m => m.meta.needAuthentication)
+  const isAuthenticated = authState.data.isAuthenticated
+
+  if (!needAuthentication && !isAuthenticated) {
+    next()
+  } else {
+    if (!needAuthentication && isAuthenticated) {
+      next()
+    } else {
+      if (needAuthentication && !isAuthenticated) {
+        next({
+          path: '/'
+        })
+      } else {
+        // Corresponds to this condition: needAuthentication && isAuthenticated)
+        next()
+      }
+    }
+  }
 })
 
 export default router
