a8bb209c7f82007e00233f3520bcf4deb2a5f532
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-07-22 12:33:33 +0200

    Add a relative path * remove minification of html files * add source-map devtool conf for better debug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: '',
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -6,5 +7,20 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  chainWebpack: config => {
+    /**
+     * Remove minification of index.html file
+     */
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].minify = false
+        /* new args to pass to html-webpack-plugin's constructor */
+        return args
+      })
   }
 }
