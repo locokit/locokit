34bbb5db9c545c16c90b91154f248a0819db433d
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-07-23 17:32:17 +0200

    Update sentry URL for lck-front

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -9,5 +9,5 @@ const LCK_SETTINGS = {
   LOGO_BG_WHITE_URL: '/img/logo-bg-white.png',
   LOGO_BG_PRIMARY_URL: '/img/logo-bg-primary.png',
   STORAGE_KEY: 'lck-auth',
-  SENTRY_DSN: 'https://c66594db39164ca7831994d0ea68d117@o421199.ingest.sentry.io/5340581'
+  SENTRY_DSN: 'https://b217f3673c9c417c82ada33ccf87a57e@sentry.makina-corpus.net/92'
 }
