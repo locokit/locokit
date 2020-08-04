9ef93e0aeec19d9797aedda620f015eed64f0384
Aline <chetta.aline@gmail.com>
2020-08-04 19:11:22 +0200

    Retrieve info for the first table on mounted

--- src/store/database.ts
+++ src/store/database.ts
@@ -22,6 +22,53 @@ export async function retrieveDatabaseByWorkspaceId (databaseId: string) {
       query: { $eager: 'tables' }
     })
     databaseState.data = result
+    return result
+  } catch (error) {
+    databaseState.error = error
+  }
+  databaseState.loading = false
+}
+
+export async function retrieveTableColumsAndTableRows (tableId: string) {
+  databaseState.loading = true
+  try {
+    return await lckClient.service('table').get(tableId, {
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      query: { $eager: '[columns, rows]' }
+    })
+  } catch (error) {
+    databaseState.error = error
+  }
+  databaseState.loading = false
+}
+
+export async function retrieveTableColumns (tableId: string) {
+  databaseState.loading = true
+  try {
+    const result = await lckClient.service('column').find({
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      query: { table_id: tableId }
+    })
+    return result.data
+  } catch (error) {
+    databaseState.error = error
+  }
+  databaseState.loading = false
+}
+
+export async function retrieveTableRows (tableId: string, pageIndex = 0) {
+  databaseState.loading = true
+  const ITEMS_PER_PAGE = 10
+
+  try {
+    return await lckClient.service('row').find({
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      query: {
+        table_id: tableId,
+        $limit: ITEMS_PER_PAGE,
+        $skip: pageIndex * ITEMS_PER_PAGE
+      }
+    })
   } catch (error) {
     databaseState.error = error
   }
--- src/views/Database.vue
+++ src/views/Database.vue
@@ -13,31 +13,53 @@
         :header="table.text"
       >
         {{ table.id }}
+        <TableView
+          :block="block"
+        />
       </p-tab-panel>
     </p-tab-view>
   </div>
 </template>
 
 <script>
-import { retrieveDatabaseByWorkspaceId, databaseState } from '@/store/database'
+import {
+  retrieveDatabaseByWorkspaceId,
+  retrieveTableRows,
+  retrieveTableColumns,
+  databaseState
+} from '@/store/database'
 import TabView from 'primevue/tabview'
 import TabPanel from 'primevue/tabpanel'
 import Vue from 'vue'
+import TableView from '@/components/visualize/TableView/TableView'
 
 export default {
   name: 'Database',
   props: ['databaseId'],
   data () {
     return {
-      databaseState
+      databaseState,
+      block: {}
     }
   },
   components: {
+    TableView,
     'p-tab-view': Vue.extend(TabView),
     'p-tab-panel': Vue.extend(TabPanel)
   },
   async mounted () {
     await retrieveDatabaseByWorkspaceId(this.databaseId)
+    this.$set(this.block, 'loading', true)
+    const definition = await retrieveTableColumns(databaseState.data?.tables[0].id)
+    const content = await retrieveTableRows(databaseState.data?.tables[0].id)
+    this.block = {
+      ...this.block,
+      definition: {
+        columns: definition
+      },
+      content
+    }
+    this.$set(this.block, 'loading', false)
   }
 }
 </script>
