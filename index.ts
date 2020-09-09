ee4af32d0041e952a82d5c5aa6fa7f635963ae93
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-09-09 12:34:52 +0200

    Use several vars and TypeScript

---
+++ index.ts
@@ -0,0 +1,26 @@
+export const COLUMN_TYPE = {
+  BOOLEAN: 1,
+  STRING: 2,
+  NUMBER: 3,
+  FLOAT: 4,
+  DATE: 5,
+  USER: 6,
+  GROUP: 7,
+  RELATION_BETWEEN_TABLES: 8,
+  LOOKED_UP_COLUMN: 9,
+  SINGLE_SELECT: 10,
+  MULTI_SELECT: 11,
+  FORMULA: 12,
+  FILE: 13
+}
+
+export const USER_PROFILE = {
+  ADMIN: 'ADMIN',
+  SUPERADMIN: 'SUPERADMIN',
+  USER: 'USER'
+}
+
+export const GROUP_ROLE = {
+  OWNER: 'OWNER',
+  MEMBER: 'MEMBER'
+}
