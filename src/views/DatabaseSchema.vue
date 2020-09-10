f9e91ba236dabcc3a5616ed64b5f1c19dbdf83a6
bastyen <bastien.alvez@makina-corpus.com>
2020-09-10 11:54:03 +0200

    use lckClient

--- src/views/DatabaseSchema.vue
+++ src/views/DatabaseSchema.vue
@@ -3,21 +3,21 @@
     <header class="p-my-4 lck-color-title p-ml-1">
       {{ $t('pages.databaseSchema.title')}}
     </header>
-    <div v-if="schema" class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
+    <div v-if="tables" class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
         <div
-          v-for="table in schema.tables"
+          v-for="table in tables"
           :key="table.id"
           class="p-shadow-8 p-pb-4 p-mx-4 p-mb-4"
           :class="{'relationIsActive' : currentRelationId === table.id}"
         >
-            <div class="schema-table-title p-px-4 p-py-2 p-text-center p-text-bold">
+            <div class="table-title p-px-4 p-py-2 p-text-center p-text-bold">
               {{ table.text }}
             </div>
             <div
               v-for="column in table.columns"
               :key="column.id"
               class="p-d-flex p-flew-row p-jc-between p-px-4 p-py-1"
-              :class="{'schema-table-column-key' : column.settings && column.settings.tableId}"
+              :class="{'table-column-key' : column.settings && column.settings.tableId}"
               @mouseover="showRelation(column.settings && column.settings.tableId)"
               @mouseleave="hideRelation"
             >
@@ -37,58 +37,54 @@
 </template>
 
 <script>
+import lckClient from '@/services/lck-api'
+
 export default {
   name: 'DatabaseSchema',
   components: {},
   props: {},
   data () {
     return {
-      schema: null,
+      tables: null,
       currentRelationId: null
     }
   },
   methods: {
     showRelation (relationId) {
       if (relationId) {
-        console.log('showRelation', relationId)
         this.currentRelationId = relationId
       }
     },
     hideRelation () {
-      this.currentRelationId = null
+      if (this.currentRelationId) {
+        this.currentRelationId = null
+      }
     }
   },
   async mounted () {
-    const token = localStorage.getItem('lck-auth')
     // eslint-disable-next-line no-undef
-    const request = LCK_SETTINGS.API_URL + '/database?$eager=tables.[columns]'
-    const headers = new Headers()
-    headers.append('Authorization', 'Bearer ' + token)
-    headers.append('content-type', 'application/json')
-    const requestInit = {
-      method: 'GET',
-      headers
+    const tablesWithColumns = await lckClient.service('table').find({
+      query: { $eager: '[columns]' }
+    })
+    if (tablesWithColumns && tablesWithColumns.data) {
+      this.tables = tablesWithColumns.data
     }
-    const schema = await fetch(request, requestInit).then((response) => response.json())
-    console.log('schema', schema)
-    // for now, by default, we use 0 as index database
-    this.schema = schema.data[0]
   }
 }
 </script>
 
 <style scoped>
-  .schema-table-title {
+  .table-title {
     background-color: #546e7a;
     color: white;
   }
-  .schema-table-column-key {
+  .table-column-key {
     cursor: pointer;
   }
-  .schema-table-column-key:hover, .relationIsActive {
-    background-color: RGBA(84, 110, 122, 0.4);
-  }
-  .schema-table-column-key:hover {
+  .table-column-key:hover {
     color: white;
+  }
+  .table-column-key:hover, .relationIsActive {
+    background-color: RGBA(84, 110, 122, 0.4);
   }
 </style>
