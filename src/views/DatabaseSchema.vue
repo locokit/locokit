ec3b4f95b7c98148ec24ed820ed9bc90eda6d5c8
bastyen <bastien.alvez@makina-corpus.com>
2020-09-11 16:23:19 +0200

    add schema visualization

--- src/views/DatabaseSchema.vue
+++ src/views/DatabaseSchema.vue
@@ -3,62 +3,41 @@
     <header class="p-my-4 lck-color-title p-ml-1">
       {{ $t('pages.databaseSchema.title')}}
     </header>
-    <div v-if="tables" class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
-        <div
-          v-for="table in tables"
-          :key="table.id"
-          class="p-shadow-8 p-pb-4 p-mx-4 p-mb-4"
-          :class="{'relationIsActive' : currentRelationId === table.id}"
-        >
-            <div class="table-title p-px-4 p-py-2 p-text-center p-text-bold">
-              {{ table.text }}
-            </div>
-            <div
-              v-for="column in table.columns"
-              :key="column.id"
-              class="p-d-flex p-flew-row p-jc-between p-px-4 p-py-1"
-              :class="{'table-column-key' : column.settings && column.settings.tableId}"
-              @mouseover="showRelation(column.settings && column.settings.tableId)"
-              @mouseleave="hideRelation"
-            >
-                <div :class="{'p-text-bold' : column.settings && column.settings.tableId}">
-                  {{ column.text }}
-                </div>
-                <div class="p-ml-2">
-                    {{ column.column_type_id }}
-                </div>
-            </div>
-        </div>
-    </div>
-    <div v-else>
-      {{ $t('pages.databaseSchema.noSchema') }}
-    </div>
+    <canvas ref="schema"></canvas>
   </div>
 </template>
 
 <script>
 import lckClient from '@/services/lck-api'
+import nomnoml from 'nomnoml'
 
 export default {
   name: 'DatabaseSchema',
-  components: {},
-  props: {},
-  data () {
-    return {
-      tables: null,
-      currentRelationId: null
-    }
-  },
   methods: {
-    showRelation (relationId) {
-      if (relationId) {
-        this.currentRelationId = relationId
-      }
-    },
-    hideRelation () {
-      if (this.currentRelationId) {
-        this.currentRelationId = null
-      }
+    createSource (tables) {
+      const sourceStyle = ['#fill: #ffffff', '#direction: right', '#ranker: longest-path']
+      const sourceTable = []
+      const sourceRelation = []
+      tables.forEach(table => {
+        if (table && table.id && table.text) {
+          const columns = []
+          table.columns.forEach(column => {
+            if (column) {
+              if (column.text) {
+                columns.push(`${column.text + (column.column_type_id ? ': ' + column.column_type_id : '')}`)
+              }
+              if (column.settings && column.settings.tableId) {
+                const relationTable = tables.find(table => table.id === column.settings.tableId)
+                if (relationTable && relationTable.text) {
+                  sourceRelation.push(`[${table.text}]-[${relationTable.text}]`)
+                }
+              }
+            }
+          })
+          sourceTable.push(`[${table.text + (columns.length ? '|' + columns.join('|') : '')}]`)
+        }
+      })
+      return sourceStyle.concat(sourceTable).concat(sourceRelation).join('\n')
     }
   },
   async mounted () {
@@ -67,24 +46,8 @@ export default {
       query: { $eager: '[columns]' }
     })
     if (tablesWithColumns && tablesWithColumns.data) {
-      this.tables = tablesWithColumns.data
+      nomnoml.draw(this.$refs.schema, this.createSource(tablesWithColumns.data))
     }
   }
 }
 </script>
-
-<style scoped>
-  .table-title {
-    background-color: #546e7a;
-    color: white;
-  }
-  .table-column-key {
-    cursor: pointer;
-  }
-  .table-column-key:hover {
-    color: white;
-  }
-  .table-column-key:hover, .relationIsActive {
-    background-color: RGBA(84, 110, 122, 0.4);
-  }
-</style>
