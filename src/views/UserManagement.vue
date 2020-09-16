75cc0cfae8223ce73be3fae28b7470828296e63c
Henri Gauffriau <henri.gauffriau@makina-corpus.com>
2020-09-16 17:17:00 +0200

    Display userList in a Datatable component

--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -6,14 +6,15 @@
       <h1>{{ $t('pages.userManagement.title') }}</h1>
     </div>
     <div v-if="users" class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
-      <ul id="list-users" class="list-none">
-        <li
-          v-for="user in users"
-          :key="user.id"
-        >
-          {{ user.first_name }} {{ user.last_name}} - {{ user.email}} - <strong>{{ user.profile }}</strong>
-        </li>
-      </ul>
+      <prime-datatable :value="users" :paginator="true" :rows="10"
+                       class="p-datatable-sm p-datatable-striped p-datatable-responsive"
+                       paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport"
+                       :currentPageReportTemplate="$t('components.paginator.currentPageReportTemplate')">
+        <prime-column field="first_name" :header="$t('pages.userManagement.firstName')"></prime-column>
+        <prime-column field="last_name" :header="$t('pages.userManagement.lastName')"></prime-column>
+        <prime-column field="email" :header="$t('pages.userManagement.email')"></prime-column>
+        <prime-column field="profile" :header="$t('pages.userManagement.profile')"></prime-column>
+      </prime-datatable>
     </div>
 
     <div v-else class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
@@ -24,13 +25,19 @@
 
 <script>
 import lckClient from '@/services/lck-api'
-
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Vue from 'vue'
 export default {
   name: 'UserManagement',
   data () {
     return {
       users: null
     }
+  },
+  components: {
+    'prime-datatable': Vue.extend(DataTable),
+    'prime-column': Vue.extend(Column)
   },
   async mounted () {
     // eslint-disable-next-line no-undef
