f52895e4b0e484fb8a6af1c4eba811630a3e588b
Henri Gauffriau <henri.gauffriau@makina-corpus.com>
2020-09-17 09:12:33 +0200

    General - add button stylesheet on project to customize specific buttons UserManagement - Add size for `id` and `tools` columns. - Add helper for DataTables alignement - Add editing and remove button on row end.

--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -8,12 +8,20 @@
     <div v-if="users" class="p-d-flex p-flex-row p-flex-wrap p-jc-start">
       <prime-datatable :value="users" :paginator="true" :rows="10"
                        class="p-datatable-sm p-datatable-striped p-datatable-responsive"
+                       :resizableColumns="true" columnResizeMode="fit"
                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport"
                        :currentPageReportTemplate="$t('components.paginator.currentPageReportTemplate')">
+        <prime-column field="id" headerClass="lck-datatable-align-center p-col-1" bodyClass="lck-datatable-align-center" :header="$t('pages.userManagement.id')" sortable></prime-column>
         <prime-column field="first_name" :header="$t('pages.userManagement.firstName')" sortable></prime-column>
         <prime-column field="last_name" :header="$t('pages.userManagement.lastName')" sortable></prime-column>
         <prime-column field="email" :header="$t('pages.userManagement.email')" sortable></prime-column>
-        <prime-column field="profile" :header="$t('pages.userManagement.profile')" sortable></prime-column>
+        <prime-column field="profile" headerClass="p-col-1"  :header="$t('pages.userManagement.profile')" sortable></prime-column>
+        <prime-column headerClass="p-col-1" bodyClass="lck-datatable-button-group">
+            <template #body="slotProps">
+            <prime-button icon="pi pi-pencil" class="p-button-rounded p-button-outlined p-mr-2" @click="editUser(slotProps.data)" />
+            <prime-button icon="pi pi-trash" class="p-button-rounded p-button-outlined" @click="confirmDeleteUser(slotProps.data)" />
+          </template>
+        </prime-column>
       </prime-datatable>
     </div>
 
@@ -28,6 +36,7 @@ import lckClient from '@/services/lck-api'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import Vue from 'vue'
+import Button from 'primevue/button'
 export default {
   name: 'UserManagement',
   data () {
@@ -35,9 +44,20 @@ export default {
       users: null
     }
   },
+  methods: {
+    editUser (user) {
+      this.user = { ...user }
+      alert(this.user.id)
+    },
+    confirmDeleteUser (user) {
+      this.user = { ...user }
+      alert(this.user.id)
+    }
+  },
   components: {
     'prime-datatable': Vue.extend(DataTable),
-    'prime-column': Vue.extend(Column)
+    'prime-column': Vue.extend(Column),
+    'prime-button': Vue.extend(Button)
   },
   async mounted () {
     // eslint-disable-next-line no-undef
