710ef80e1e69ae13c17ccd6ed7bfc079af1c5a90
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-09-28 17:09:01 +0200

    Add test for trr

---
+++ src/services/trr/trr.test.ts
@@ -0,0 +1,10 @@
+import app from '../../app';
+
+describe('\'trr\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('trr');
+    expect(service).toBeTruthy();
+  });
+
+
+});
