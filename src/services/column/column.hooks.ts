f21c1d263aa7edcb228e0ff309d0a6b355775f89
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-09-28 17:08:56 +0200

    Disable pagination for columns with table_id, if $limit set to -1

--- src/services/column/column.hooks.ts
+++ src/services/column/column.hooks.ts
@@ -1,10 +1,17 @@
 import * as authentication from '@feathersjs/authentication';
+import { disablePagination, discardQuery, iff } from 'feathers-hooks-common';
+import { queryContainsKey } from '../../hooks/lck-hooks/queryContainsKey';
 const { authenticate } = authentication.hooks;
 
 export default {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
+    find: [
+      iff(
+        queryContainsKey('table_id'),
+        disablePagination()
+      )
+    ],
     get: [],
     create: [],
     update: [],
