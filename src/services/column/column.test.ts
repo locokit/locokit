f676bace13e0a0a54b755d3519454207fb68d65a
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-09-28 17:43:47 +0200

    Add test for pagination

--- src/services/column/column.test.ts
+++ src/services/column/column.test.ts
@@ -21,4 +21,42 @@ describe('\'column\' service', () => {
 
     expect(tableColumn).toBeTruthy()
   });
+
+  it('paginate results by default to 10', async () => {
+    expect.assertions(2);
+    const service = app.service('column');
+    const columns: any = await service.find()
+    expect(columns.total).toBeDefined()
+    expect(columns.limit).toBe(10)
+  });
+
+  it('discard pagination when table_id is set in query param and $limit is -1', async () => {
+    expect.assertions(5);
+    const service = app.service('column');
+    const workspace = await app.service('workspace').create({text: 'pouet'})
+    const database = await app.service('database').create({text: 'pouet', workspace_id: workspace.id})
+    const table = await app.service('table').create({text: 'pouet', database_id: database.id})
+    const column1 = await service.create({
+      text: 'myColumn1',
+      table_id: table.id,
+      column_type_id: COLUMN_TYPE.STRING
+    })
+    const column2 = await service.create({
+      text: 'myColumn2',
+      table_id: table.id,
+      column_type_id: COLUMN_TYPE.STRING
+    })
+
+    const columns: any = await service.find({
+      query: {
+        table_id: table.id,
+        $limit: -1
+      }
+    })
+    expect(columns.total).not.toBeDefined()
+    expect(columns.limit).not.toBeDefined()
+    expect(columns.length).toBe(2)
+    expect(columns[0].id).toBe(column1.id)
+    expect(columns[1].id).toBe(column2.id)
+  });
 });
