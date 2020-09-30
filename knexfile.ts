e47aee07dcb777632d45b1478d3169656e0ef3ad
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-09-30 11:09:38 +0200

    Adapt test for local environment, with NODE_ENV=test

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   client: "pg",
-  connection: process.env.LCK_DATABASE_URL,
+  connection: process.env.NODE_ENV === 'test' ? process.env.LCK_DATABASE_URL_TEST : process.env.LCK_DATABASE_URL,
   migrations: {
     tableName: 'knex_migrations',
     extension: 'ts'
