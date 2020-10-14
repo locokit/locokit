668f60d5b7abb186d143a7be8e52c93920422d6d
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-14 12:02:55 +0200

    Update sentry #51

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from 'winston'
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
@@ -10,7 +10,7 @@ const logger = createLogger({
   ),
   transports: [
     new transports.Console()
-  ],
-});
+  ]
+})
 
-export default logger;
+export default logger
