14e45fde90795bd819c2d25a6e626d9d435f96a7
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-15 10:12:29 +0200

    Add constraint + tests

--- src/services/table/table.test.ts
+++ src/services/table/table.test.ts
@@ -5,4 +5,44 @@ describe('\'table\' service', () => {
     const service = app.service('table')
     expect(service).toBeTruthy()
   })
+
+  it('create a table on the right database', async () => {
+    const workspace = await app.services.workspace.create({
+      text: 'myWorkspace'
+    })
+    const db = await app.services.database.create({
+      text: 'myDB',
+      workspace_id: workspace.id
+    })
+    const table = await app.services.table.create({
+      text: 'myTable',
+      database_id: db.id
+    })
+    expect(table).toBeTruthy()
+    expect(table.id).toBeDefined()
+    await app.services.table.remove(table.id)
+    await app.services.database.remove(db.id)
+    await app.services.workspace.remove(workspace.id)
+  })
+
+  it('throw an error if we create a table with a name already taken, on the same database', async () => {
+    const workspace = await app.services.workspace.create({
+      text: 'myWorkspace'
+    })
+    const db = await app.services.database.create({
+      text: 'myDB',
+      workspace_id: workspace.id
+    })
+    const table = await app.services.table.create({
+      text: 'myTable',
+      database_id: db.id
+    })
+    await expect(app.services.table.create({
+      text: 'myTable',
+      database_id: db.id
+    })).rejects.toThrow()
+    await app.services.table.remove(table.id)
+    await app.services.database.remove(db.id)
+    await app.services.workspace.remove(workspace.id)
+  })
 })
