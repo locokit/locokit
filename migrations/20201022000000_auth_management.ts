1bc17384ec1e79866d1f7e95a75533706c4e8307
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-22 19:42:23 +0200

    Upgrade user model

---
+++ migrations/20201022000000_auth_management.ts
@@ -0,0 +1,27 @@
+import * as Knex from 'knex'
+
+export async function up (knex: Knex): Promise<any> {
+  return knex.schema
+    .alterTable('user', function (table) {
+      table.boolean('isVerified').defaultTo(false)
+      table.string('verifyToken')
+      table.string('verifyShortToken')
+      table.date('verifyExpires')
+      table.boolean('verifyChanges').defaultTo(false)
+      table.string('resetToken')
+      table.date('resetExpires')
+    })
+}
+
+export async function down (knex: Knex): Promise<any> {
+  return knex.schema
+    .alterTable('user', function (table) {
+      table.dropColumn('isVerified')
+      table.dropColumn('verifyToken')
+      table.dropColumn('verifyShortToken')
+      table.dropColumn('verifyExpires')
+      table.dropColumn('verifyChanges')
+      table.dropColumn('resetToken')
+      table.dropColumn('resetExpires')
+    })
+}
