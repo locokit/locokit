ebdca26928f65524abad549f58bccc414b6ef28a
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-23 15:43:37 +0200

    Better TS definition

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,4 @@
-import { Application as ExpressFeathers } from '@feathersjs/express';
+import { Application as ExpressFeathers } from '@feathersjs/express'
 
 // A mapping of service names to types. Will be extended in service files.
 export interface ServiceTypes {}
