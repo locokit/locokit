6b5d8bbecc924a207ae19b82f68698f022a46f17
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-27 09:19:47 +0100

    Add a 404 page

---
+++ src/views/routes/404.stories.js
@@ -0,0 +1,16 @@
+import { action } from '@storybook/addon-actions'
+
+import Page404 from './404.vue'
+
+export default {
+  title: 'routes/404',
+  component: Page404
+}
+
+export const defaultStory = () => ({
+  components: { Page404 },
+  template: '<Page404 @submit="this.submit" />',
+  methods: { submit: action('submit') }
+})
+
+defaultStory.storyName = 'default'
