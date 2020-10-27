1f4f7b7d130f48da081c8c5de0271e48120d37a6
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-27 09:17:58 +0100

    Update wrapper storybook

--- .storybook/ThemeWrapper.vue
+++ .storybook/ThemeWrapper.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="h-full p-fluid"><slot/></div>
+  <div style="height: 100%" class="p-fluid">
+    <slot/>
+  </div>
 </template>
+
+<script>
+import '../src/plugins/primevue'
+
+export default {
+  name: 'StorybookThemeWrapper'
+}
+</script>
 
 <style src="../public/assets/css/theme.css"></style>
 
 <style>
-#root {
+#root,
+html,
+body {
   height: 100%;
+  padding: unset !important;
+  margin: unset !important;
+}
+</style>
+<style lang="scss" src="@/styles/main.scss"></style>
+
+<style lang="scss" scoped>
+header {
+  height: 4rem;
 }
 </style>
