e6e1a3086e80c1223f23093cf0b344a840216543
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-10-30 12:07:11 +0100

    Fix calendar for CrudTable #133

--- src/components/store/CrudTable/CrudTable.stories.js
+++ src/components/store/CrudTable/CrudTable.stories.js
@@ -1,5 +1,6 @@
 import CrudTable from './CrudTable'
 import { COLUMN_TYPE } from '@locokit/lck-glossary'
+import Vue from 'vue'
 
 export default {
   title: 'components/store/CrudTable',
@@ -35,7 +36,6 @@ const tableViewData = {
     columns: [
       {
         text: 'Nom du vélo',
-        data: {},
         id: 'e065323c-1151-447f-be0f-6d2728117b38',
         settings: null,
         table_id: '163c21e6-5339-4748-903f-8c77e21314cf',
@@ -49,7 +49,6 @@ const tableViewData = {
         }
       }, {
         text: 'État du vélo',
-        data: {},
         id: '3a659ea1-446f-4755-8db9-583a204279cc',
         settings: {
           values: {
@@ -81,11 +80,23 @@ const tableViewData = {
         }
       }, {
         text: 'Fournisseur',
-        data: {},
         id: 'bde4bbbd-2584-447f-acff-f434f53619da',
         settings: null,
         table_id: '163c21e6-5339-4748-903f-8c77e21314cf',
         column_type_id: COLUMN_TYPE.USER,
+        order: null,
+        filter: null,
+        visible: true,
+        type: {
+          text: 'User',
+          id: 5
+        }
+      }, {
+        text: 'Date',
+        id: 'b9058b15-44b7-4cd9-a121-a942b69a0434',
+        settings: null,
+        table_id: '163c21e6-5339-4748-903f-8c77e21314cf',
+        column_type_id: COLUMN_TYPE.DATE,
         order: null,
         filter: null,
         visible: true,
@@ -104,6 +115,7 @@ const tableViewData = {
         text: 'Vélo n° 42',
         data: {
           '3a659ea1-446f-4755-8db9-583a204279cc': 1,
+          'b9058b15-44b7-4cd9-a121-a942b69a0434': '2020-10-30',
           'bde4bbbd-2584-447f-acff-f434f53619da': {
             value: 'AMSTERDAMAIR',
             reference: 4
@@ -118,6 +130,7 @@ const tableViewData = {
         text: 'Vélo n° YYYY',
         data: {
           '3a659ea1-446f-4755-8db9-583a204279cc': 2,
+          'b9058b15-44b7-4cd9-a121-a942b69a0434': '2020-10-30',
           'bde4bbbd-2584-447f-acff-f434f53619da': {
             value: 'CYCLABLE ENTREPRISE',
             reference: 3
@@ -164,3 +177,29 @@ export const CrudTableWithPropsStory = () => (
 )
 
 CrudTableWithPropsStory.storyName = 'CrudTable with expected props'
+
+export const crudModeWithCalendar = () => (
+  {
+    components: { CrudTable },
+    data () {
+      return {
+        block: tableViewData
+      }
+    },
+    template: '<CrudTable :block="block" :crud-mode="true" ref="lck-datatable" />',
+    async mounted () {
+      const crudElement = this.$refs['lck-datatable'].$el
+      await Vue.nextTick()
+      const calendarCellFirstRow = crudElement.querySelector('table > tbody > tr > td:nth-child(4)')
+      calendarCellFirstRow.click()
+      await Vue.nextTick()
+
+      const nextMonthButton = document.querySelector('.p-datepicker-next.p-link')
+      console.log(nextMonthButton)
+      nextMonthButton && nextMonthButton.click()
+    }
+  }
+)
+
+crudModeWithCalendar.storyName = 'with calendar opened and month changed'
+crudModeWithCalendar.args = { timeoutBeforeScreenshot: 1000 }
