31b34348193a009160918c00a84fb13503db423d
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-11-09 15:35:25 +0100

    Fix process model

--- migrations/20201022000000_process.ts
+++ migrations/20201022000000_process.ts
@@ -19,8 +19,10 @@ export async function up (knex: Knex): Promise<any> {
       table.jsonb('settings')
       table.timestamp('createdAt').defaultTo('now()')
       table.timestamp('updatedAt').defaultTo('now()')
-      table.uuid('process_id').unsigned()
-      table.foreign('process_id', 'FK_pt_process_id').references('id').inTable('workspace')
+      table.uuid('process_id').unsigned().notNullable()
+      table.foreign('process_id', 'FK_pt_process_id').references('id').inTable('process')
+      table.uuid('table_id').unsigned()
+      table.foreign('table_id', 'FK_pt_table_id').references('id').inTable('table')
     })
 
     .createTable('process_execution', table => {
@@ -33,10 +35,10 @@ export async function up (knex: Knex): Promise<any> {
       table.jsonb('settings')
       table.timestamp('createdAt').defaultTo('now()')
       table.timestamp('updatedAt').defaultTo('now()')
-      table.uuid('process_id').unsigned()
-      table.foreign('process_id', 'FK_pe_process_id').references('id').inTable('workspace')
-      table.uuid('process_trigger_id').unsigned()
-      table.foreign('process_trigger_id', 'FK_pe_pt_id').references('id').inTable('workspace')
+      table.uuid('process_trigger_id').unsigned().notNullable()
+      table.foreign('process_trigger_id', 'FK_pe_pt_id').references('id').inTable('process_trigger')
+      table.uuid('table_row_id').unsigned()
+      table.foreign('table_row_id', 'FK_pe_table_row_id').references('id').inTable('table_row')
     })
 }
 
