a5e80fb1ad1eb7f1e782bf654b0dd262876e4e16
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-11-12 17:28:31 +0100

    Don't send sentry errors on dev

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -4,21 +4,21 @@ import * as Sentry from '@sentry/browser'
 import { Vue as VueIntegration } from '@sentry/integrations'
 import { Integrations } from '@sentry/tracing'
 
-// if (process.env.NODE_ENV === 'production') {
-Sentry.init({
-  dsn: LCK_SETTINGS.SENTRY_DSN,
-  integrations: [
-    new Integrations.BrowserTracing(),
-    new VueIntegration({
-      Vue,
-      tracing: true,
-      tracingOptions: {
-        trackComponents: true,
-        timeout: 100,
-        hooks: ['create', 'mount', 'update', 'destroy']
-      }
-    })
-  ],
-  tracesSampleRate: 1.0 // Be sure to lower this in production
-})
-// }
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: LCK_SETTINGS.SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new VueIntegration({
+        Vue,
+        tracing: true,
+        tracingOptions: {
+          trackComponents: true,
+          timeout: 100,
+          hooks: ['create', 'mount', 'update', 'destroy']
+        }
+      })
+    ],
+    tracesSampleRate: 1.0 // Be sure to lower this in production
+  })
+}
