ff81b679253a3a6af1fc9b7e581decca1e211a9b
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-11-13 08:59:26 +0100

    Add lckServices for knowing in advance all endpoints available

--- src/services/lck-api/index.ts
+++ src/services/lck-api/index.ts
@@ -20,7 +20,27 @@ lckClient.configure(auth({
 // })
 
 export const lckServices = {
-  database: lckClient.service('database')
+  /**
+   * Database part
+   */
+  database: lckClient.service('database'),
+  table: lckClient.service('table'),
+  tableColumn: lckClient.service('column'),
+  tableView: lckClient.service('view'),
+  tableRow: lckClient.service('row'),
+  tableViewColumn: lckClient.service('table-view-has-table-column'),
+  /**
+   * Visualization
+   */
+  visuChapter: lckClient.service('chapter'),
+  visuPage: lckClient.service('page'),
+  visuContainer: lckClient.service('container'),
+  visuBlock: lckClient.service('block'),
+  /**
+   * User
+   */
+  user: lckClient.service('user'),
+  group: lckClient.service('group')
 }
 
 export default lckClient
