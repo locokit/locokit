2d3261e1e21097e96ece0266f86531eb7c3aeab9
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-11-20 16:08:54 +0100

    Remove permission

--- src/services/permission/permission.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../app'
-
-describe('\'permission\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('permission')
-    expect(service).toBeTruthy()
-  })
-})
