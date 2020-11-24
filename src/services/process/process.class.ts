10af8f696c6b4571f374e6566e3e23035fb5a728
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-11-24 22:13:18 +0100

    use of Model in service definition * process, trigger and execution

--- src/services/process/process.class.ts
+++ src/services/process/process.class.ts
@@ -1,11 +1,12 @@
 import { Service, ObjectionServiceOptions } from 'feathers-objection'
 import { Application } from '../../declarations'
+import { Process as ProcessModel } from '../../models/process.model'
 
 interface Options extends ObjectionServiceOptions {
   Model: any;
 }
 
-export class Process extends Service {
+export class Process extends Service<ProcessModel> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor (options: Partial<Options>, app: Application) {
     const { Model, ...otherOptions } = options
--- src/services/process_trigger/process_trigger.class.ts
+++ src/services/process_trigger/process_trigger.class.ts
@@ -1,11 +1,12 @@
 import { Service, ObjectionServiceOptions } from 'feathers-objection'
 import { Application } from '../../declarations'
+import { ProcessTrigger as ProcessTriggerModel } from '../../models/process_trigger.model'
 
 interface Options extends ObjectionServiceOptions {
   Model: any;
 }
 
-export class ProcessTrigger extends Service {
+export class ProcessTrigger extends Service<ProcessTriggerModel> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor (options: Partial<Options>, app: Application) {
     const { Model, ...otherOptions } = options
