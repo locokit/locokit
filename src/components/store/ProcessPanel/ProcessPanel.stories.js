13a1a9ec5054843a08db67405c3a1dbfed2668bd
Aline <chetta.aline@gmail.com>
2020-11-27 13:44:24 +0100

    Update story + snapshot

--- src/components/store/ProcessPanel/ProcessPanel.stories.js
+++ src/components/store/ProcessPanel/ProcessPanel.stories.js
@@ -24,40 +24,62 @@ export const withProcessesByRowAndRowId = () => (
           {
             id: 'uuid-v4-t1',
             text: 'Trigger 1',
-            event: 'MANUAL',
+            trigger: 'MANUAL',
             enabled: 'true',
+            maximumNumberSuccess: 1,
             table_id: 'uuid-v4-table-1',
-            executions: [
+            runs: [
               {
-                id: 'uuid-v4-run-1',
-                text: 'Run 1',
-                trigger_id: 'uuid-v4-t1',
+                id: 'uuid-v4-t1-run-1',
+                text: 'Run 3',
+                process_id: 'uuid-v4-t1',
                 createdAt: '2020-11-22T22:00:00Z',
                 status: 'SUCCESS',
                 duration: 100,
-                table_row_id: 17,
+                table_row_id: '17',
                 log: 'This is the log'
               }, {
-                id: 'uuid-v4-run-2',
+                id: 'uuid-v4-t1-run-2',
+                text: 'Run 2',
+                process_id: 'uuid-v4-t1',
+                createdAt: '2020-11-22T22:00:00Z',
+                status: 'WARNING',
+                duration: null,
+                table_row_id: '17',
+                log: 'This is the log'
+              }, {
+                id: 'uuid-v4-t1-run-3',
                 text: 'Run 1',
-                trigger_id: 'uuid-v4-t1',
+                process_id: 'uuid-v4-t1',
                 createdAt: '2020-11-22T22:00:00Z',
                 status: 'ERROR',
                 duration: null,
-                table_row_id: 17,
+                table_row_id: '17',
                 log: 'This is the log'
               }
             ]
           }, {
             id: 'uuid-v4-t2',
             text: 'Trigger 2',
-            event: 'CRON',
+            trigger: 'CRON',
             enabled: 'true',
             table_id: 'uuid-v4-table-1'
           }, {
             id: 'uuid-v4-t3',
             text: 'Trigger 3',
-            event: 'CREATE_ROW',
+            trigger: 'CREATE_ROW',
+            runs: [
+              {
+                id: 'uuid-v4-t3-run-1',
+                text: 'Run 1',
+                process_id: 'uuid-v4-t3',
+                createdAt: '2020-11-22T22:00:00Z',
+                status: 'RUNNING',
+                duration: 100,
+                table_row_id: '17',
+                log: 'This is the log'
+              }
+            ],
             enabled: 'true',
             table_id: 'uuid-v4-table-1'
           }
