7acabe24a43dff4bd14c0d5d0b086ebae2fc7532
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-12-13 22:26:09 +0100

    Update code & shots

--- src/components/admin/database/LookedUpTypeColumn/LookedUpTypeColumn.stories.js
+++ src/components/admin/database/LookedUpTypeColumn/LookedUpTypeColumn.stories.js
@@ -8,7 +8,7 @@ export default {
 export const createStory = () => (
   {
     components: { LookedUpTypeColumn },
-    template: '<LookedUpTypeColumn />'
+    template: '<div class="p-fluid"><LookedUpTypeColumn /></div>'
   }
 )
 
--- src/components/admin/database/LookedUpTypeColumn/LookedUpTypeColumn.vue
+++ src/components/admin/database/LookedUpTypeColumn/LookedUpTypeColumn.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
-    <div class="p-d-flex">
-      <div class="p-ai-start p-mt-4">
-        <Label for="local-field-id">{{ $t('pages.databaseSchema.lookedUpType.localField') }}</Label>
-        <p-dropdown id="local-field-id" :disabled="!Boolean(this.localFields.length)" appendTo="body" v-model="localFieldId" @change="onLocalFieldChange" :options="localFields" dataKey="id" optionValue="id" optionLabel="text" :placeholder="$t('pages.databaseSchema.lookedUpType.localFieldPlaceholder')" />
-      </div>
-    </div>
-    <div class="p-d-flex">
-      <div class="p-ai-start p-mt-4">
-        <Label for="foreign-field-id">{{ $t('pages.databaseSchema.lookedUpType.foreignField') }}</Label>
-        <p-dropdown id="foreign-field-id" :disabled="!Boolean(this.foreignFields.length)" appendTo="body" v-model="foreignFieldId" :options="foreignFields" dataKey="id" optionValue="id" optionLabel="text" :placeholder="$t('pages.databaseSchema.lookedUpType.foreignFieldPlaceholder')" />
-      </div>
-    </div>
+    <label for="local-field-id" class="p-mt-4 p-d-block">
+      {{ $t('pages.databaseSchema.lookedUpType.localField') }}
+    </label>
+    <p-dropdown
+      id="local-field-id"
+      :disabled="!Boolean(this.localFields.length)"
+      appendTo="body"
+      v-model="localFieldId"
+      @change="onLocalFieldChange"
+      :options="localFields"
+      dataKey="id"
+      optionValue="id"
+      optionLabel="text"
+      :placeholder="$t('pages.databaseSchema.lookedUpType.localFieldPlaceholder')"
+    />
+    <label for="foreign-field-id" class="p-mt-4 p-d-block">
+      {{ $t('pages.databaseSchema.lookedUpType.foreignField') }}
+    </label>
+    <p-dropdown
+      id="foreign-field-id"
+      :disabled="!Boolean(this.foreignFields.length)"
+      appendTo="body"
+      v-model="foreignFieldId"
+      :options="foreignFields"
+      dataKey="id"
+      optionValue="id"
+      optionLabel="text"
+      :placeholder="$t('pages.databaseSchema.lookedUpType.foreignFieldPlaceholder')"
+    />
   </div>
 </template>
-<script>
+
+<script lang="ts">
 import Vue from 'vue'
 import { COLUMN_TYPE } from '@locokit/lck-glossary'
 import { lckClient } from '@/services/lck-api'
@@ -47,20 +65,18 @@ export default {
       const localFields = await lckClient.service('column').find({
         query: {
           // eslint-disable-next-line @typescript-eslint/camelcase
-          database_id: this.databaseId,
+          column_type_id: COLUMN_TYPE.RELATION_BETWEEN_TABLES,
           // eslint-disable-next-line @typescript-eslint/camelcase
           table_id: this.tableId,
           $limit: 100
         }
       })
-      this.localFields = localFields?.data.filter((column) => column.column_type_id === COLUMN_TYPE.RELATION_BETWEEN_TABLES)
+      this.localFields = localFields?.data
     },
     async loadForeignFields () {
       if (this.relationTableId) {
         const foreignFields = await lckClient.service('column').find({
           query: {
-            // eslint-disable-next-line @typescript-eslint/camelcase
-            database_id: this.databaseId,
             // eslint-disable-next-line @typescript-eslint/camelcase
             table_id: this.relationTableId,
             $limit: 100
@@ -84,13 +100,13 @@ export default {
     this.loadForeignFields()
   },
   watch: {
-    localFieldId: function () {
+    localFieldId () {
       this.$emit('local-field-id-change', this.localFieldId)
     },
-    foreignFieldId: function () {
+    foreignFieldId () {
       this.$emit('foreign-field-id-change', this.foreignFieldId)
     },
-    relationTableId: function () {
+    relationTableId () {
       this.$emit('relation-table-id-change', this.relationTableId)
     }
   }
