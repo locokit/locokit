a8a1340090b50e2cd38d54092a6b327c0c07485a
bastyen <bastien.alvez@makina-corpus.com>
2021-01-20 13:08:31 +0100

    add synthesis block

---
+++ src/components/visualize/Synthesis/Synthesis.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>{{ this.title }}</h4>
+    <div v-if="result" class="p-grid">
+      <div class="p-col-12 lck-color-page-title">
+        <h2>{{ this.result }}</h2>
+      </div>
+    </div>
+    <div v-else>
+      {{ this.noResult }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Synthesis',
+  props: {
+    title: "",
+    result: "",
+    noResult: ""
+  }
+})
+</script>
