66e1df9383ca3c8ee05b4f41ee056293212be114
Julien Cougnaud <jco@makina-corpus.net>
2021-02-03 12:02:08 +0100

    [crud-visualization-page] Use the dialog form component

--- src/components/visualize/PageDialog/PageDialog.vue
+++ src/components/visualize/PageDialog/PageDialog.vue
@@ -1,46 +1,40 @@
 <template>
-  <lck-dialog
-    :visible="visible"
+  <lck-dialog-form
     :header="page.id ? $t('pages.workspace.editPage') : $t('pages.workspace.createPage')"
+    :submitting="submitting"
+    :visible="visible"
     @close="$emit('close')"
+    @input="$emit('input', { text: pageTextCopy, hidden: pageHiddenCopy })"
   >
-    <lck-form
-      :submitting="submitting"
-      @submit="$emit('input', { text: pageTextCopy, hidden: pageHiddenCopy })"
-      @cancel="$emit('close')"
-    >
-      <div class="p-field">
-        <label for="pageTextField">{{ $t('pages.workspace.pageName') }}</label>
-        <p-input-text
-          id="pageTextField"
-          v-model="pageTextCopy"
-          required
-          autofocus
-        />
-      </div>
-      <div class="p-field">
-        <label for="PageHiddenField">{{ $t('pages.workspace.pageHidden') }}</label>
-        <p-input-switch
-          id="PageHiddenField"
-          v-model="pageHiddenCopy"
-        />
-      </div>
-    </lck-form>
-  </lck-dialog>
+    <div class="p-field">
+      <label for="pageTextField">{{ $t('pages.workspace.pageName') }}</label>
+      <p-input-text
+        id="pageTextField"
+        v-model="pageTextCopy"
+        required
+        autofocus
+      />
+    </div>
+    <div class="p-field">
+      <label for="PageHiddenField">{{ $t('pages.workspace.pageHidden') }}</label>
+      <p-input-switch
+        id="PageHiddenField"
+        v-model="pageHiddenCopy"
+      />
+    </div>
+  </lck-dialog-form>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import LckForm from '@/components/ui/Form/Form.vue'
-import LckDialog from '@/components/ui/Dialog/Dialog.vue'
+import LckDialogForm from '@/components/ui/DialogForm/DialogForm.vue'
 import InputText from 'primevue/inputtext'
 import InputSwitch from 'primevue/inputswitch'
 
 export default {
   name: 'PageDialog',
   components: {
-    'lck-form': LckForm,
-    'lck-dialog': LckDialog,
+    'lck-dialog-form': LckDialogForm,
     'p-input-text': Vue.extend(InputText),
     'p-input-switch': Vue.extend(InputSwitch)
   },
