ca929a7655747a989144c1f6944cb6198d8fce30
bastyen <bastien.alvez@makina-corpus.com>
2021-02-08 12:53:14 +0100

    add two css classes

--- src/components/visualize/Synthesis/Synthesis.vue
+++ src/components/visualize/Synthesis/Synthesis.vue
@@ -2,8 +2,8 @@
   <div>
     <h4>{{ this.title }}</h4>
     <div v-if="result" class="p-grid">
-      <div class="p-col-12 lck-color-page-title">
-        <h2>{{ this.result }}</h2>
+      <div class="p-col-12 lck-synthesis">
+        <p class="lck-synthesis-result">{{ this.result }}</p>
       </div>
     </div>
     <div v-else>
@@ -33,3 +33,15 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+.lck-synthesis {
+  color: var(--primary-color);
+}
+.lck-synthesis-result {
+  font-size: 1.5em;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  font-weight: bold;
+}
+</style>
