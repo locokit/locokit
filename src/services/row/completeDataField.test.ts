8506dd8751b965d7393250af11aeae0c71f4d827
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-02-23 18:41:09 +0100

    Add test for complete data field

---
+++ src/services/row/completeDataField.test.ts
@@ -0,0 +1,103 @@
+import { COLUMN_TYPE } from '@locokit/lck-glossary'
+import app from '../../app'
+import { TableColumn } from '../../models/tablecolumn.model'
+import { database } from '../../models/database.model'
+import { TableRow } from '../../models/tablerow.model'
+import { table } from '../../models/table.model'
+import { User } from '../../models/user.model'
+import { workspace } from '../../models/workspace.model'
+
+describe('completeDataField hook', () => {
+  let workspace: workspace
+  let database: database
+  let table1: table
+  let table2: table
+  let columnTable1Ref: TableColumn
+  let columnTable1User: TableColumn
+  let user1: User
+  let rowTable1: TableRow
+  let columnTable1FirstName: TableColumn
+  let columnTable1LastName: TableColumn
+
+  beforeAll(async () => {
+    workspace = await app.service('workspace').create({ text: 'pouet' })
+    database = await app.service('database').create({ text: 'pouet', workspace_id: workspace.id })
+    table1 = await app.service('table').create({
+      text: 'table1',
+      database_id: database.id
+    })
+    table2 = await app.service('table').create({
+      text: 'table2',
+      database_id: database.id
+    })
+    columnTable1Ref = await app.service('column').create({
+      text: 'Ref',
+      column_type_id: COLUMN_TYPE.STRING,
+      table_id: table1.id
+    })
+    columnTable1User = await app.service('column').create({
+      text: 'User',
+      column_type_id: COLUMN_TYPE.USER,
+      table_id: table1.id
+    })
+    columnTable1FirstName = await app.service('column').create({
+      text: 'FirstName',
+      column_type_id: COLUMN_TYPE.STRING,
+      table_id: table1.id,
+      reference: true,
+      reference_position: 1
+    })
+    columnTable1LastName = await app.service('column').create({
+      text: 'LastName',
+      column_type_id: COLUMN_TYPE.STRING,
+      table_id: table1.id,
+      reference: true,
+      reference_position: 2
+    })
+    user1 = await app.service('user').create({
+      name: 'User 1',
+      email: 'user1-data-field@locokit.io',
+      password: 'locokit'
+    })
+    rowTable1 = await app.service('row').create({
+      table_id: table1.id,
+      text: 'table 1 ref',
+      data: {
+        [columnTable1Ref.id]: 'this is a ref',
+        [columnTable1FirstName.id]: 'first name',
+        [columnTable1LastName.id]: 'last name',
+        [columnTable1User.id]: user1.id
+      }
+    })
+  })
+
+  it('complete data field when patching it', async () => {
+    const rowTablePatched = await app.service('row').patch(rowTable1.id, {
+      data: {
+        [columnTable1FirstName.id]: 'new firstname'
+      }
+    })
+    expect.assertions(4)
+    expect(rowTablePatched.data[columnTable1FirstName.id]).toBe('new firstname')
+    expect(rowTablePatched.data[columnTable1LastName.id]).toBe('last name')
+    expect(rowTablePatched.data[columnTable1User.id]).toStrictEqual({
+      reference: user1.id,
+      value: user1.name
+    })
+    expect(rowTablePatched.data[columnTable1Ref.id]).toBe('this is a ref')
+    await app.service('row').remove(rowTable1.id)
+  })
+
+  afterAll(async () => {
+    await app.service('row').remove(rowTable1.id)
+    await app.service('user').remove(user1.id)
+    await app.service('column').remove(columnTable1LastName.id)
+    await app.service('column').remove(columnTable1FirstName.id)
+    await app.service('column').remove(columnTable1User.id)
+    await app.service('column').remove(columnTable1Ref.id)
+    await app.service('table').remove(table1.id)
+    await app.service('table').remove(table2.id)
+    await app.service('database').remove(database.id)
+    await app.service('workspace').remove(workspace.id)
+  })
+})
