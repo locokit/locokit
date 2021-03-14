a5bdf3f998ec9a5e383245ec426b86a888f5163a
Aline Chetta <aline.chetta@makina-corpus.com>
2021-03-14 20:15:23 +0100

    Add case Formula in column

--- src/services/lck-utils/columns.ts
+++ src/services/lck-utils/columns.ts
@@ -1,10 +1,7 @@
-import { COLUMN_TYPE } from '@locokit/lck-glossary'
-
-interface Column {
-  column_type_id: number;
-  editable: boolean;
-  parents?: Column[];
-}
+import {
+  Column,
+  COLUMN_TYPE
+} from '@locokit/lck-glossary'
 
 export function getComponentEditableColumn (columnTypeId: number) {
   switch (columnTypeId) {
@@ -50,6 +47,7 @@ export function isEditableColumn (crudMode: boolean, column: Column) {
 }
 
 export function columnAncestor (column: Column): COLUMN_TYPE {
+  if (column.column_type_id === COLUMN_TYPE.FORMULA) return column.settings.formula_type_id
   if (column.column_type_id !== COLUMN_TYPE.LOOKED_UP_COLUMN || (column.parents && column.parents.length === 0) || !column.parents) {
     return column.column_type_id
   }
