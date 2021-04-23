25f7a4a9c1e3111395a8354b0bacd54b61d7715d
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-04-23 10:28:07 +0200

    update CI

--- scripts/compileTemplate.js
+++ scripts/compileTemplate.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 const nunjucks = require('nunjucks')
 const fs = require('fs')
 
 console.log('Creating environment variables')
 const envVars = {
   lck_app_title: process.env.VUE_APP_TITLE,
-  lck_app_theme: process.env.VUE_APP_THEME,
+  lck_app_theme: process.env.VUE_APP_THEME
 }
 console.log('Compiling dist/index.html')
 const indexHTML = nunjucks.render('dist/index.html', envVars)
