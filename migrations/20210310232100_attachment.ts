83700275b61ccc1b40b797dd97d7deae0426348b
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-05-04 00:26:29 +0200

    Remove row_has_attachment

--- migrations/20210310232100_attachment.ts
+++ migrations/20210310232100_attachment.ts
@@ -14,17 +14,9 @@ export async function up (knex: Knex): Promise<void> {
       table.uuid('workspace_id').unsigned().notNullable()
       table.foreign('workspace_id', 'FK_attachment_workspace_id').references('id').inTable('workspace')
     })
-    .createTable('row_has_attachment', table => {
-      table.increments('id')
-      table.uuid('table_row_id').unsigned()
-      table.foreign('table_row_id', 'FK_rha_table_row_id').references('id').inTable('table_row')
-      table.integer('attachment_id')
-      table.foreign('attachment_id', 'FK_rha_attachment_id').references('id').inTable('attachment')
-    })
 }
 
 export async function down (knex: Knex): Promise<void> {
   return knex.schema
-    .dropTableIfExists('row_has_attachment')
     .dropTableIfExists('attachment')
 }
