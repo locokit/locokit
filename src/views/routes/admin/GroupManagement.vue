9f392b5031ed28761f8b2fe128b19a77da11f81e
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-06-09 13:58:50 +0200

    Add translation for GM workspace https://gitlab.makina-corpus.net/lck/lck-front/-/merge_requests/222#note_274922

--- src/views/routes/admin/GroupManagement.vue
+++ src/views/routes/admin/GroupManagement.vue
@@ -16,7 +16,7 @@
           <h2>{{ group.name }}</h2>
           <span class="p-pl-1">
             (
-              Espace de travail <strong>{{ group.aclset.workspace.text }}</strong>,
+              {{ $t('pages.groupManagement.text.workspace') }}<strong>{{ group.aclset.workspace.text }}</strong>,
               <span v-if="group.aclset.chapter">
                 chapitre <strong>{{ group.aclset.chapter.text }}</strong>
               </span>
