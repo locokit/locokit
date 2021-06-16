3f992dd44fa92121885c6f325329b53c06d652c2
Aline Chetta <aline.chetta@makina-corpus.com>
2021-06-16 08:56:08 +0200

    Update lib locokit + Migration to rename or add block

---
+++ migrations/20210611102748_rename_block.ts
@@ -0,0 +1,186 @@
+import * as Knex from "knex";
+
+const formatAlterTableEnumSql = (
+  tableName: string,
+  columnName: string,
+  enums: string[]
+) => {
+  const constraintName = `${tableName}_${columnName}_check`
+  console.log(constraintName, enums)
+  return [
+    `ALTER TABLE ${tableName} DROP CONSTRAINT IF EXISTS ${constraintName};`,
+    `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
+      '\'::text, \''
+    )}'::text]));`
+  ].join('\n')
+}
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(
+    formatAlterTableEnumSql('block', 'type', [
+      'TableView',
+      'TableSet',
+      'DetailView',
+      'DataRecord',
+      'Paragraph',
+      'Markdown',
+      'Heading',
+      'Media',
+      'KanbanView',
+      'KanbanSet',
+      'GridView',
+      'MapView',
+      'MapSet',
+      'Synthesis',
+      'HighlightField',
+      'MapDetailView',
+      'MapField',
+      'ActionButton',
+      'Default'
+    ])
+  )
+  await knex('block')
+    .where('type', 'TableView')
+    .update({
+      type: 'TableSet'
+    })
+  await knex('block')
+    .where('type', 'DetailView')
+    .update({
+      type: 'DataRecord'
+    })
+  await knex('block')
+    .where('type', 'Heading')
+    .update({
+      type: 'Default'
+    })
+  await knex('block')
+    .where('type', 'KanbanView')
+    .update({
+      type: 'KanbanSet'
+    })
+  await knex('block')
+    .where('type', 'Synthesis')
+    .update({
+      type: 'HighlightField'
+    })
+  await knex('block')
+    .where('type', 'GridView')
+    .update({
+      type: 'Default'
+    })
+  await knex('block')
+    .where('type', 'MapView')
+    .update({
+      type: 'MapSet'
+    })
+  await knex('block')
+    .where('type','MapDetailView')
+    .update({
+      type: 'MapField'
+    })
+
+  await knex.raw(
+    formatAlterTableEnumSql('block', 'type', [
+      'TableSet',
+      'DataRecord',
+      'Paragraph',
+      'Markdown',
+      'Media',
+      'KanbanSet',
+      'MapSet',
+      'MapField',
+      'HighlightField',
+      'ActionButton',
+      'CardSet',
+      'MarkdownField',
+      'FormRecord',
+      'Default'
+    ])
+  )
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(
+    formatAlterTableEnumSql('block', 'type', [
+      'TableView',
+      'TableSet',
+      'DetailView',
+      'DataRecord',
+      'Paragraph',
+      'Markdown',
+      'Heading',
+      'Media',
+      'KanbanView',
+      'KanbanSet',
+      'GridView',
+      'MapView',
+      'MapSet',
+      'Synthesis',
+      'HighlightField',
+      'MapDetailView',
+      'MapField',
+      'ActionButton',
+      'Default'
+    ])
+  )
+  await knex('block')
+    .where('type', 'TableSet')
+    .update({
+      type: 'TableView'
+    })
+  await knex('block')
+    .where('type', 'DataRecord')
+    .update({
+      type: 'DetailView'
+    })
+  await knex('block')
+    .where('type', 'KanbanSet')
+    .update({
+      type: 'KanbanView'
+    })
+  await knex('block')
+    .where('type', 'Map')
+    .update({
+      type: 'MapView'
+    })
+  await knex('block')
+    .where('type', 'MapField')
+    .update({
+      type: 'MapDetailView'
+    })
+  await knex('block')
+    .where('type', 'CardSet')
+    .update({
+      type: 'Default'
+    })
+  await knex('block')
+    .where('type', 'MarkdownField')
+    .update({
+      type: 'Default'
+    })
+  await knex('block')
+    .where('type', 'FormRecord')
+    .update({
+      type: 'Default'
+    })
+  await knex.raw(
+    formatAlterTableEnumSql('block', 'type', [
+      'TableView',
+      'DetailView',
+      'Paragraph',
+      'Markdown',
+      'Heading',
+      'Media',
+      'KanbanView',
+      'GridView',
+      'MapView',
+      'Synthesis',
+      'MapDetailView',
+      'ActionButton',
+      'Default'
+    ])
+  )
+}
+
--- src/models/block.model.ts
+++ src/models/block.model.ts
@@ -5,6 +5,21 @@ import { JSONSchema, snakeCaseMappers } from 'objection'
 import { Application } from '../declarations'
 import { BaseModel } from './base.model'
 
+// TableSet: Table (header + content) of a set of record
+// DataRecord: Detail of each data (label + input/text) of a record
+// Paragraph: Any text
+// Markdown: Any text with markdown style
+// Media: Image Collection which can be in shape of Gallery, Carousel and Image or a Video in a player
+// KanbanSet: Method Kanban on a set of record
+// MapSet: Map with one or many set of record and one or many geographical data
+// MapField: Map with one record and one geographical data
+// HighlightField: Highlighting a field in a record
+// ActionButton: Button which can be related on a field in a record
+// CardSet: Collection of card from a set of record
+// MarkdownField: Text related on a field in a record with markdown style
+// FormRecord: Same as DataRecord with at least submission button
+// Default: Safe-keeper (migration backwards compatibility)
+
 export class block extends BaseModel {
   title?: string
   container_id!: string
@@ -35,7 +50,24 @@ export class block extends BaseModel {
           type: 'string',
           format: 'uuid',
         },
-        type: { enum: ['TableView', 'DetailView', 'Paragraph', 'Markdown', 'Heading', 'Media', 'KanbanView', 'GridView', 'MapView', 'Synthesis', 'MapDetailView', 'ActionButton', 'Default'] },
+        type: {
+          enum: [
+            'TableSet',
+            'DataRecord',
+            'Paragraph',
+            'Markdown',
+            'Media',
+            'KanbanSet',
+            'HighlightField',
+            'MapSet',
+            'MapField',
+            'ActionButton',
+            'CardSet',
+            'MarkdownField',
+            'FormRecord',
+            'Default',
+          ],
+        },
         position: { type: ['number', 'null'] },
         settings: { type: ['object', 'null'] },
         elevation: { type: 'boolean' },
