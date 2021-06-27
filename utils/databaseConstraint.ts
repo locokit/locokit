23a7825b35d48294189f32ac821f223223a62666
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-06-28 01:46:56 +0200

    [block-conditional-display] Fix migration helper

--- utils/databaseConstraint.ts
+++ utils/databaseConstraint.ts
@@ -13,8 +13,8 @@ export const formatAlterTableEnumSql = (
 ): string => {
   const constraintName = `${tableName}_${columnName}_check`
   return [
-    `ALTER TABLE ${tableName} DROP CONSTRAINT IF EXISTS ${constraintName};`,
-    `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
+    `ALTER TABLE "${tableName}" DROP CONSTRAINT IF EXISTS ${constraintName};`,
+    `ALTER TABLE "${tableName}" ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
       '\'::text, \'',
     )}'::text]));`,
   ].join('\n')
