48087d332174e2762028840da99f553aba54cafa
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-07-05 23:57:38 +0200

    [form-default-values] First iteration

--- src/components/visualize/FormRecord/FormRecord.spec.js
+++ src/components/visualize/FormRecord/FormRecord.spec.js
@@ -16,7 +16,7 @@ const mockDefinitionsWithRequiredColumns = {
       editable: false,
       position: 1,
       transmitted: false,
-      default: '',
+      default: null,
       displayed: true,
       required: true,
       sort: 'DESC',
@@ -33,7 +33,7 @@ const mockDefinitionsWithRequiredColumns = {
       editable: false,
       position: 1,
       transmitted: false,
-      default: '',
+      default: null,
       displayed: true,
       required: true,
       sort: 'DESC',
@@ -50,7 +50,7 @@ const mockDefinitionsWithRequiredColumns = {
       editable: false,
       position: 1,
       transmitted: false,
-      default: '',
+      default: null,
       displayed: true,
       required: true,
       sort: 'DESC',
@@ -96,6 +96,124 @@ const mockDefinitionsWithoutRequiredColumns = {
       sort: 'DESC',
       table_column_id: '',
       table_view_id: 'table_view_2',
+      style: {},
+    },
+  ],
+}
+
+const mockDefinitionsWithDefaultColumns = {
+  id: 'table_view_3',
+  columns: [
+    {
+      text: 'Description',
+      id: 'string_3_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.STRING,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: {
+        value: 'Default string value',
+      },
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
+      style: {},
+    },
+    {
+      text: 'Number',
+      id: 'number_3_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.NUMBER,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: {
+        value: 0,
+      },
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
+      style: {},
+    },
+    {
+      text: 'Boolean',
+      id: 'boolean_3_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.BOOLEAN,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: {
+        value: true,
+      },
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
+      style: {},
+    },
+    {
+      text: 'String related 1',
+      id: 'string_4_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.STRING,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: {
+        fieldId: 'string_3_column',
+      },
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
+      style: {},
+    },
+    {
+      text: 'String related 2',
+      id: 'string_5_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.STRING,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: null,
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
+      style: {},
+    },
+    {
+      text: 'String related 3',
+      id: 'string_6_column',
+      settings: {},
+      table_id: 'table_3',
+      column_type_id: COLUMN_TYPE.STRING,
+      editable: false,
+      position: 1,
+      transmitted: false,
+      default: {
+        fieldId: 'string_5_column',
+      },
+      displayed: true,
+      required: true,
+      sort: 'DESC',
+      table_column_id: '',
+      table_view_id: 'table_view_3',
       style: {},
     },
   ],
@@ -152,7 +270,7 @@ describe('FormRecord', () => {
         await wrapper.setProps({
           definition: mockDefinitionsWithRequiredColumns,
         })
-        wrapper.setData({
+        await wrapper.setData({
           newRow: {
             data: {
               string_1_column: 'Data',
@@ -167,7 +285,7 @@ describe('FormRecord', () => {
         await wrapper.setProps({
           definition: mockDefinitionsWithRequiredColumns,
         })
-        wrapper.setData({
+        await wrapper.setData({
           newRow: {
             data: {
               string_1_column: '',
@@ -183,7 +301,7 @@ describe('FormRecord', () => {
         await wrapper.setProps({
           definition: mockDefinitionsWithRequiredColumns,
         })
-        wrapper.setData({
+        await wrapper.setData({
           newRow: {
             data: {
               string_1_column: 'Data',
@@ -193,6 +311,36 @@ describe('FormRecord', () => {
           },
         })
         expect(wrapper.vm.completeForm).toBe(true)
+      })
+    })
+
+    describe('default values', () => {
+      it('Set initial values with default ones', async () => {
+        await wrapper.setProps({
+          definition: mockDefinitionsWithDefaultColumns,
+        })
+        expect(wrapper.vm.newRow).toMatchObject({
+          data: {
+            string_3_column: 'Default string value',
+            number_3_column: 0,
+            boolean_3_column: true,
+            string_4_column: 'Default string value',
+          },
+        })
+        await wrapper.vm.onUpdateRow({
+          columnId: 'string_5_column',
+          newValue: 'This is a test to be duplicated',
+        })
+        expect(wrapper.vm.newRow).toMatchObject({
+          data: {
+            string_3_column: 'Default string value',
+            number_3_column: 0,
+            boolean_3_column: true,
+            string_4_column: 'Default string value',
+            string_5_column: 'This is a test to be duplicated',
+            string_6_column: 'This is a test to be duplicated',
+          },
+        })
       })
     })
   })
