55e1d101f72b014f4f1aad65855d6e1a9cf3482a
Julien Cougnaud <jco@makina-corpus.net>
2021-08-30 15:07:24 +0200

    [virtual-looked-up-column] Only make one API request by table when getting the foreign data instead of one by row

--- src/services/row/computeRowVirtualLookedUpColumns.hook.ts
+++ src/services/row/computeRowVirtualLookedUpColumns.hook.ts
@@ -14,8 +14,6 @@ export function computeRowVirtualLookedUpColumns (): Hook {
     ) {
       return context
     }
-    // Save data to prevent redundant DB requests
-    const foreignRows: Record<string, TableRow> = {}
     // First, encapsulate the row(s) in an array to harmonize the computation
     let resultRows: TableRow[] = []
     if (Array.isArray(context.result)) {
@@ -28,38 +26,101 @@ export function computeRowVirtualLookedUpColumns (): Hook {
       // Single row
       resultRows = [context.result]
     }
+    // Store the rows with VIRTUAL_LOOKED_UP_COLUMN columns and the configuration of these specific columns
+    const resultRowsWithVLUC: Record<string, {
+      row: TableRow
+      virtualColumns: Record<string, {
+        foreignTable: string
+        foreignColumn: string
+        foreignRow: string
+      }>
+    }> = {}
+    const foreignRowsIdsByTable: Record<string, string[]> = {}
     // Loop on result rows
-    for (const resultRow of resultRows) {
-      if (!resultRow.data) continue
-      // Compute all the VIRTUAL_LOOKED_UP_COLUMN columns of the current row
-      for (const column of context.params._meta.columns as TableColumn[]) {
-        if (column.column_type_id === COLUMN_TYPE.VIRTUAL_LOOKED_UP_COLUMN && column.settings?.localField) {
+    for (const column of context.params._meta.columns as TableColumn[]) {
+      // Get the VIRTUAL_LOOKED_UP_COLUMN columns which are well configured
+      if (
+        column.column_type_id === COLUMN_TYPE.VIRTUAL_LOOKED_UP_COLUMN &&
+        column.settings?.localField &&
+        column.settings.foreignField
+      ) {
+        // Get the foreign table id
+        const foreignTableId = (context.params._meta.columns as TableColumn[])
+          .find(c => c.id === column.settings.localField)
+          ?.settings.tableId
+
+        if (!foreignTableId) continue
+
+        for (const resultRow of resultRows) {
           // Id of the linked row (through the RELATION_BETWEEN_TABLES column linked to the current one)
           const foreignRowId = (resultRow.data[column.settings.localField] as { reference: string, value: string } | undefined)?.reference
-          // Id of the linked column
-          const foreignColumnId = column.settings.foreignField
-          // Valid configuration
-          if (foreignRowId && foreignColumnId) {
-            try {
-              // Get the foreign row
-              if (!foreignRows[foreignRowId]) {
-                foreignRows[foreignRowId] = await context.service.get(foreignRowId, {
-                  _meta: {
-                    computeVirtualLookedUpColumn: true, // Needed to compute the foreign virtual looked-up columns
-                  },
-                })
-              }
-              // Include the value of the VIRTUAL_LOOKED_UP_COLUMN into the row data
-              resultRow.data[column.id] = foreignRows[foreignRowId].data[foreignColumnId]
-            } catch (e) {
-              if (e instanceof Error) {
-                e.message = `Impossible to retrieve the data from the column '${column.id}'. ${e.message}`
-              }
-              throw e
+
+          if (!foreignRowId) continue
+
+          // Get the foreign rows to load (a set of rows for each table to load)
+          if (!foreignRowsIdsByTable[foreignTableId]) {
+            foreignRowsIdsByTable[foreignTableId] = []
+          }
+          foreignRowsIdsByTable[foreignTableId].push(foreignRowId)
+
+          // Get a list of the rows with the VIRTUAL_LOOKED_UP_COLUMN columns configurations
+          if (!resultRowsWithVLUC[resultRow.id]) {
+            resultRowsWithVLUC[resultRow.id] = {
+              row: resultRow,
+              virtualColumns: {},
             }
+          }
+          resultRowsWithVLUC[resultRow.id].virtualColumns[column.id] = {
+            foreignTable: foreignTableId,
+            foreignColumn: column.settings.foreignField,
+            foreignRow: foreignRowId,
           }
         }
       }
+    }
+
+    try {
+      const tablesToLoadIds = Object.keys(foreignRowsIdsByTable)
+      if (tablesToLoadIds.length > 0) {
+        // For each foreign table, get the rows to load from the database
+        const foreignRowsByTable: Record<string, Record<string, TableRow>> = (await Promise.all(
+          tablesToLoadIds.map(async tableId => await context.service.find({
+            query: {
+              id: {
+                $in: foreignRowsIdsByTable[tableId],
+              },
+              table_id: tableId,
+            },
+            paginate: false,
+            _meta: {
+              computeVirtualLookedUpColumn: true, // Needed to compute the foreign VIRTUAL_LOOKED_UP_COLUMN columns
+            },
+          }) as TableRow[]),
+        )).reduce((rowsByTable: Record<string, Record<string, TableRow>>, tableRows, currentIndex) => {
+          rowsByTable[tablesToLoadIds[currentIndex]] = tableRows.reduce((rows: Record<string, TableRow>, row) => {
+            rows[row.id] = row
+            return rows
+          }, {})
+          return rowsByTable
+        }, {})
+
+        // Compute all the VIRTUAL_LOOKED_UP_COLUMN columns for each row
+        for (const resultRowId in resultRowsWithVLUC) {
+          const resultRow = resultRowsWithVLUC[resultRowId]
+          for (const columnId in resultRow.virtualColumns) {
+            const columnConfiguration = resultRow.virtualColumns[columnId]
+            // Include the value of the VIRTUAL_LOOKED_UP_COLUMN into the row data
+            const foreignData = foreignRowsByTable[columnConfiguration.foreignTable][columnConfiguration.foreignRow]
+              ?.data[columnConfiguration.foreignColumn]
+            if (foreignData) resultRow.row.data[columnId] = foreignData
+          }
+        }
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        e.message = `Impossible to compute the virtual values. ${e.message}`
+      }
+      throw e
     }
     return context
   }
