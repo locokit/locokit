2eec3383abb299c8673c96386f5ccf85fb49c1d6
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-09-30 21:34:55 +0200

    Add new component ExternalApp for iframe

---
+++ src/components/visualize/ExternalApp/ExternalApp.vue
@@ -0,0 +1,25 @@
+<template>
+  <iframe
+    :src="src"
+  />
+</template>
+
+<script>
+export default {
+  name: 'ExternalApp',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+iframe {
+  border: unset;
+  width: 100%;
+  height: 100%;
+}
+</style>
