a7ced39082ce904bbe2d2dabda5a7499951d937c
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-10-25 14:31:35 +0200

    Update settings to default empty object

--- migrations/20211025142000_add_workspace_settings.ts
+++ migrations/20211025142000_add_workspace_settings.ts
@@ -3,7 +3,7 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema
     .alterTable('workspace', table => {
-      table.jsonb('settings')
+      table.jsonb('settings').notNullable().defaultTo('{}')
     })
 }
 
