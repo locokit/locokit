5c8705795424166552ad9fcdfab24f860f1ea283
mdartic <mathieu@dartic.fr>
2021-11-09 14:38:32 +0000

    Add changes

---
+++ assets/user-manual_create-a-database.md.6f7ffdf4.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t}from"./app.033d0cf9.js";const f='{"title":"","description":"","frontmatter":{},"relativePath":"user-manual/create-a-database.md","lastUpdated":1636468676448}',r={};function s(c,o,n,p,d,_){return t(),e("div")}var l=a(r,[["render",s]]);export{f as __pageData,l as default};
---
+++ assets/user-manual_create-application.md.9e33de68.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as e}from"./app.033d0cf9.js";const l='{"title":"","description":"","frontmatter":{},"relativePath":"user-manual/create-a-database.md","lastUpdated":1636468676448}',r={};function c(o,s,n,p,d,i){return e(),t("div")}var f=a(r,[["render",c]]);export{l as __pageData,f as default};
