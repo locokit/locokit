9875cdf741121da33bdf07b8f5b1f2b44d473cd9
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-11-09 01:59:52 +0100

    [docs] Update configuration

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,64 +9,65 @@ module.exports = {
       apiKey: '3105e30092957574fc3729f68ef8dbc5',
       indexName: 'locokit'
     },
-    sidebar: {
-      '/admin-manual/': [{
-        text: 'Admin manual',
-        children: [{
-          link: '/admin-manual/auth',
-          text: 'Authentication'
-        }, {
-          link: '/admin-manual/permissions',
-          text: 'Permissions'
-        }, {
-          link: '/admin-manual/database',
-          text: 'Database'
-        }, {
-          link: '/admin-manual/cicd',
-          text: 'CI / CD'
-        }, {
-          link: '/admin-manual/configure',
-          text: 'Configuration'
-        }, {
-          link: '/admin-manual/visualization',
-          text: 'Visualization'
-        }, {
-          link: '/admin-manual/webhooks',
-          text: 'Web hooks'
-        }]
-      }],
-      '/developer-manual/': [{
-        text: 'Developer manual',
-        children: [{
-          text: 'Front end',
-          link: '/developer-manual/frontend'
-        }, {
-          text: 'Storybook',
-          link: '/developer-manual/storybook'
-        }, {
-          text: 'Back end',
-          link: '/developer-manual/backend'
-        }]
-      }]
-    },
+    // sidebar: {
+    //   '/admin-manual/': [{
+    //     text: 'Admin manual',
+    //     children: [{
+    //       link: '/admin-manual/auth',
+    //       text: 'Authentication'
+    //     }, {
+    //       link: '/admin-manual/permissions',
+    //       text: 'Permissions'
+    //     }, {
+    //       link: '/admin-manual/database',
+    //       text: 'Database'
+    //     }, {
+    //       link: '/admin-manual/cicd',
+    //       text: 'CI / CD'
+    //     }, {
+    //       link: '/admin-manual/configure',
+    //       text: 'Configuration'
+    //     }, {
+    //       link: '/admin-manual/visualization',
+    //       text: 'Visualization'
+    //     }, {
+    //       link: '/admin-manual/webhooks',
+    //       text: 'Web hooks'
+    //     }]
+    //   }],
+    //   '/developer-manual/': [{
+    //     text: 'Developer manual',
+    //     children: [{
+    //       text: 'Front end',
+    //       link: '/developer-manual/frontend'
+    //     }, {
+    //       text: 'Storybook',
+    //       link: '/developer-manual/storybook'
+    //     }, {
+    //       text: 'Back end',
+    //       link: '/developer-manual/backend'
+    //     }]
+    //   }]
+    // },
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Manuals', items: [
-        {
-          text: 'Administrator',
-          link: '/admin-manual/'
-        },
-        {
-          text: 'Developer',
-          link: '/developer-manual/'
-        },
-        {
-          text: 'User',
-          link: '/user-manual/'
-        }
-      ]},
+      // { text: 'Manuals', items: [
+      //   {
+      //     text: 'Administrator',
+      //     link: '/admin-manual/'
+      //   },
+      //   {
+      //     text: 'Developer',
+      //     link: '/developer-manual/'
+      //   },
+      //   {
+      //     text: 'User',
+      //     link: '/user-manual/'
+      //   }
+      // ]},
       { text: 'Storybook Front', link: 'http://lck-storybook.surge.sh' },
-      { text: 'Repo gitlab', link: 'https://gitlab.com/locokit/' },
+      { text: 'GitHub', link: 'https://github.com/locokit/locokit' },
+      { text: 'Docker', link: 'https://hub.docker.com/r/locokit/locokit/tags' },
     ],
   },
 }
