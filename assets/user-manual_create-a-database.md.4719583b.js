451701698a4a8f914970576286a5b788d1cf51aa
mdartic <mathieu@dartic.fr>
2021-12-30 09:48:44 +0000

    Add changes

---
+++ assets/user-manual_create-a-database.md.4719583b.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t}from"./app.78152875.js";const l='{"title":"","description":"","frontmatter":{},"relativePath":"user-manual/create-a-database.md","lastUpdated":1640857691007}',r={};function s(c,o,n,p,_,d){return t(),e("div")}var f=a(r,[["render",s]]);export{l as __pageData,f as default};
