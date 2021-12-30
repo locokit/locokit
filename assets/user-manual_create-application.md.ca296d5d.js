39c9afbcc3367d5f86147093e577a0f05381bcd4
mdartic <mathieu@dartic.fr>
2021-12-30 10:21:04 +0000

    Add changes

---
+++ assets/user-manual_create-application.md.ca296d5d.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as e}from"./app.78152875.js";const l='{"title":"","description":"","frontmatter":{},"relativePath":"user-manual/create-a-database.md","lastUpdated":1640859567106}',r={};function c(o,s,n,p,i,_){return e(),t("div")}var f=a(r,[["render",c]]);export{l as __pageData,f as default};
