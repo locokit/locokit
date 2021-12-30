b14fcab56b0c4b56a3bc940ba20294b691932533
mdartic <mathieu@dartic.fr>
2021-12-30 09:58:33 +0000

    Add changes

---
+++ assets/admin-manual_configure.md.c4e6563c.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as a,d as t}from"./app.78152875.js";const f='{"title":"Sentry","description":"","frontmatter":{},"relativePath":"admin-manual/configure.md","lastUpdated":1640858277247}',s={},o=t("",10),n=[o];function i(d,c,l,_,p,h){return a(),r("div",null,n)}var E=e(s,[["render",i]]);export{f as __pageData,E as default};
---
+++ assets/admin-manual_webhooks.md.200a36e0.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,d as o}from"./app.78152875.js";const m='{"title":"Webhooks","description":"","frontmatter":{},"relativePath":"admin-manual/webhooks.md","lastUpdated":1640858277255}',i={},r=o("",6),n=[r];function s(l,c,d,h,p,_){return t(),a("div",null,n)}var f=e(i,[["render",s]]);export{m as __pageData,f as default};
