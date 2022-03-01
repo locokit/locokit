deb0d5ed20d5dc278cc8b18a65d19d2c87149c7f
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-03-02 00:40:27 +0100

    Trying to set the front version with $GITHUB_REF_NAME

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
-process.env.VUE_APP_VERSION = require('./package.json').version
+// process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
   publicPath: '/',
