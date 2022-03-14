05ed3e93ceda495e5e6c5f7b7b4ad875d36cfb6a
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-03-14 14:11:58 +0100

    Update snaps

--- api/src/services/signup/signup.service.ts
+++ api/src/services/signup/signup.service.ts
@@ -16,8 +16,8 @@ export default function (app: Application): void {
   const signupConfig = app.get('authentication').signup
   if (signupConfig.isAllowed === 'true') {
     const signUpRateLimiter = rateLimiter(
-      parseInt(signupConfig.rateLimit.max, 10),
-      parseInt(signupConfig.rateLimit.timeframe, 10),
+      parseInt(signupConfig.rateLimit.max, 10) || 10,
+      parseInt(signupConfig.rateLimit.timeframe, 10) || 60000,
     )
     // Initialize our service with any options it requires
     app.use('/signup', signUpRateLimiter, new SignUp(app))
