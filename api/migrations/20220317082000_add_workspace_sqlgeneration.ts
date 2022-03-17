91740eda2480381ee00751ac63de5b71c7f4852f
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-03-17 18:36:36 +0100

    Add migrations for constraint, snake case and not null

--- api/migrations/20220317082000_add_workspace_sqlgeneration.ts
+++ api/migrations/20220317082000_add_workspace_sqlgeneration.ts
@@ -2,24 +2,27 @@ import * as Knex from 'knex'
 
 export async function up (knex: Knex): Promise<void> {
   await knex.schema
+    .raw(`CREATE DOMAIN lck_snake_case AS TEXT
+      CHECK(
+         VALUE ~ '^[a-z_]$'
+      );
+    `)
     .alterTable('workspace', table => {
-      table.string('slug', 255)
+      table.boolean('generate_sql').defaultTo(false)
+      table.specificType('slug', 'lck_snake_case')
       table.unique(['slug'])
     })
+    .raw('ALTER TABLE workspace ADD CONSTRAINT workspace_slug_notnull_if_generatesqltrue CHECK ( NOT ( generate_sql = True AND slug IS NULL ) );')
     .alterTable('database', table => {
-      table.string('slug', 255)
+      table.specificType('slug', 'lck_snake_case')
       table.unique(['slug', 'workspace_id'])
     })
     .alterTable('table', table => {
-      table.string('slug', 255)
+      table.specificType('slug', 'lck_snake_case')
       table.unique(['slug', 'database_id'])
     })
     .alterTable('table_column', table => {
-      table.string('slug', 255)
-      table.unique(['slug', 'table_id'])
-    })
-    .alterTable('table_view', table => {
-      table.string('slug', 255)
+      table.specificType('slug', 'lck_snake_case')
       table.unique(['slug', 'table_id'])
     })
 }
@@ -27,23 +30,17 @@ export async function up (knex: Knex): Promise<void> {
 export async function down (knex: Knex): Promise<void> {
   await knex.schema
     .alterTable('workspace', table => {
-      table.dropUnique(['slug'])
+      table.dropColumn('generate_sql')
       table.dropColumn('slug')
     })
     .alterTable('database', table => {
-      table.dropUnique(['slug', 'workspace_id'])
       table.dropColumn('slug')
     })
     .alterTable('table', table => {
-      table.dropUnique(['slug', 'database_id'])
       table.dropColumn('slug')
     })
     .alterTable('table_column', table => {
-      table.dropUnique(['slug', 'table_id'])
       table.dropColumn('slug')
     })
-    .alterTable('table_view', table => {
-      table.dropUnique(['slug', 'table_id'])
-      table.dropColumn('slug')
-    })
+    .raw('DROP DOMAIN lck_snake_case;')
 }
