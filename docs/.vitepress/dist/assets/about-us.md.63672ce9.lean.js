aef9a4b2f4654a8d15f125917624ae517eeb47e4
mdartic <mathieu@dartic.fr>
2022-05-04 07:00:20 +0000

    Remove old files

--- docs/.vitepress/dist/assets/about-us.md.63672ce9.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as o,o as t,d as e}from"./app.ee1354cf.js";var s="/autonomens.png",r="/makina.svg";const g='{"title":"About us","description":"","frontmatter":{},"relativePath":"about-us.md","lastUpdated":1651613030151}',n={},i=e("",6),_=[i];function p(c,l,u,d,m,h){return t(),o("div",null,_)}var v=a(n,[["render",p]]);export{g as __pageData,v as default};
