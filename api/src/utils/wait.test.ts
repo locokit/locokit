bde9ffc3453febd2ec78394eff23d06d1f53d8e9
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-05-10 22:36:36 +0200

    [quality] Fix 1 code smell

--- api/src/utils/wait.test.ts
+++ api/src/utils/wait.test.ts
@@ -2,7 +2,7 @@
  * Util to wait a certain amount of time, with a Promise syntax
  */
 export async function wait (duration: number): Promise<unknown> {
-  return await new Promise(resolve => {
+  return new Promise(resolve => {
     setTimeout(resolve, duration)
   })
 }
