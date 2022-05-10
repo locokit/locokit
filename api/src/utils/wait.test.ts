c19e73db17dd2c3abee859d7456a6327285dd805
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-05-10 23:57:21 +0200

    [quality] Fix eslint error

--- api/src/utils/wait.test.ts
+++ api/src/utils/wait.test.ts
@@ -2,7 +2,7 @@
  * Util to wait a certain amount of time, with a Promise syntax
  */
 export async function wait (duration: number): Promise<unknown> {
-  return new Promise(resolve => {
+  return await new Promise(resolve => {
     setTimeout(resolve, duration)
   })
 }
