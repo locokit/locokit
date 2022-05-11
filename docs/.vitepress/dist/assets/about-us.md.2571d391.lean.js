4a0e1ac432c55d8307f570576131838645bfc7d4
mdartic <mathieu@dartic.fr>
2022-05-11 07:46:54 +0000

    Add changes

---
+++ docs/.vitepress/dist/assets/about-us.md.2571d391.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as o,o as t,d as e}from"./app.5ea13208.js";var s="/autonomens.png",r="/makina.svg";const g='{"title":"About us","description":"","frontmatter":{},"relativePath":"about-us.md","lastUpdated":1652255186606}',n={},i=e("",6),_=[i];function p(c,l,u,d,m,h){return t(),o("div",null,_)}var v=a(n,[["render",p]]);export{g as __pageData,v as default};
