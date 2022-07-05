b02aad5850a1a6cafc44430d9a7ee6431700ec15
Aline Chetta <aline.chetta@makina-corpus.com>
2022-07-05 17:25:27 +0200

    :art: Add missing translations + Add workflow to check it

---
+++ front/scripts/compareFileTranslation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+fileTranslationFR="./src/locales/fr.json"
+fileTranslationEN="./src/locales/en.json"
+
+# For each file we retrieve all keys (nested included) then all key is flatten
+getAllNestedKeyFlatten="[leaf_paths as \$path | {\"key\": \$path | join(\".\"), \"value\": getpath(\$path)}] | from_entries | keys_unsorted"
+
+keysFR=$(jq "$getAllNestedKeyFlatten" $fileTranslationFR)
+lengthKeysFR=$(jq "$keysFR | length" $fileTranslationFR)
+echo "For file ${fileTranslationFR}: Total = ${lengthKeysFR}"
+
+keysEN=$(jq "$getAllNestedKeyFlatten" $fileTranslationEN)
+lengthKeysEN=$(jq "$keysEN | length" $fileTranslationEN)
+echo "For file ${fileTranslationEN}: Total = ${lengthKeysEN}"
+
+# Compare file
+echo
+if [[ lengthKeysFR -ne lengthKeysEN ]]; then
+  echo "Diff between file"
+  diff --color -s <(jq "$getAllNestedKeyFlatten" $fileTranslationFR) <(jq "$getAllNestedKeyFlatten" $fileTranslationEN)
+else
+  echo "No missing translations"
+fi
