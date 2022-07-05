3dfef8ba43461229cb76e47d12e541214252d190
Aline Chetta <aline.chetta@makina-corpus.com>
2022-07-05 16:33:27 +0200

    Add explanation

--- front/src/services/lck-utils/temporary.ts
+++ front/src/services/lck-utils/temporary.ts
@@ -1,7 +1,11 @@
 import { COLUMN_TYPE } from '@locokit/lck-glossary'
 // Todo: To refacto in glossary
 
-export function transformColumnsType (): Record<string, number> {
+/**
+ * Transform COLUMN_TYPE's enum in record
+ * Avoid key and value inversion and multiplication of data when we use directly this enum
+ */
+export function transformColumnsType (): Record<string, string> {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   return Object.entries(COLUMN_TYPE).reduce((acc: Record<string, any>, elem: Record<string, any>) => {
     if (!isNaN(elem[0])) {
--- front/src/services/lck-utils/transformText.ts
+++ front/src/services/lck-utils/transformText.ts
@@ -1,10 +1,13 @@
+/**
+ * Allow to create a slug in snake_case from a string
+ */
 export const createSlug = (value: string | null) => {
   if (!value) return ''
 
   // https://stackoverflow.com/a/37511463
   return value
-    .toLocaleLowerCase()
-    .normalize('NFD')
+    .toLocaleLowerCase() // Convert to lower case
+    .normalize('NFD') // Return Unicode Normalization Form
     .replace(/[\u0300-\u036f]/g, '') // Remove accent
     .replace(/\s{2,}/, ' ') // Remove multiple space to one
     .replace(/ /g, '_') // Replace space by underscore
