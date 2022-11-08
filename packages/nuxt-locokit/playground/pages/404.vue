53dc7416f5e79cfb89e3fc8f7a92e404255368f6
Romain Boissieres <romain.boissieres@makina-corpus.com>
2022-11-08 15:04:49 +0100

    Remove route params

--- packages/nuxt-locokit/playground/pages/404.vue
+++ packages/nuxt-locokit/playground/pages/404.vue
@@ -1,9 +1,20 @@
 <template>
   <WithBackground>
-    <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 text-center">
-      <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $t('pages.404.title') }}</h1>
-      <p class="mb-7 font-normal text-gray-700 dark:text-gray-400">{{ $route.params.slug }}</p>
-      <router-link to="/" class="p-button p-component" aria-label="Back to Homepage" ><span class="pi pi-sign-in p-button-icon p-button-icon-left"></span><span class="p-button-label">{{ $t('pages.404.link') }}</span><span class="p-ink" role="presentation"></span></router-link>
+    <div
+      class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 text-center"
+    >
+      <h1
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        {{ $t('pages.404.title') }}
+      </h1>
+      <router-link to="/" class="p-button p-component">
+        <span
+          class="pi pi-sign-in p-button-icon p-button-icon-left"
+          aria-hidden="true"
+        ></span>
+        <span class="p-button-label">{{ $t('pages.404.link') }}</span>
+      </router-link>
     </div>
   </WithBackground>
 </template>
