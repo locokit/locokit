2d14ff97e36a3d78cc923b4ae49ab9d62a24eea5
Mathieu DARTIGUES <mathieu@dartic.fr>
2022-11-15 21:38:12 +0100

    Update db initialisation + use of Objection

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,6 +1,7 @@
 import knex from 'knex'
 import type { Knex } from 'knex'
 import type { Application } from './declarations'
+import { Model } from 'objection'
 
 declare module './declarations' {
   interface Configuration {
@@ -10,7 +11,19 @@ declare module './declarations' {
 
 export function db(app: Application): void {
   const config = app.get('settings').db
-  const db = knex(config)
+  const db = knex({
+    client: config.client,
+    useNullAsDefault: false,
+    debug: true,
+    connection: {
+      connectionString: config.connection,
+      application_name: 'LocoKit',
+    },
+    // debug: process.env.OBJECTION_DEBUG === 'true' || false,
+    // pool: { min: 0, max: 20 },
+  })
+
+  Model.knex(db)
 
   app.set('db', db)
 }
