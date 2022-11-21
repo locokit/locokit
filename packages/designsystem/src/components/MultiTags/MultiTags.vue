0a0dce628ab119e9a3df683a1663d9fa2d74bd09
Romain Boissieres <romain.boissieres@makina-corpus.com>
2022-11-21 10:53:23 +0100

    Github#177 Gitlab#178 Migrate badge and multibadge in SingleTag and multiTags componenents

---
+++ packages/designsystem/src/components/MultiTags/MultiTags.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <TagLabel
+      v-for="option in options"
+      :key="`${option.label}-${option.value}`"
+      :background-color="option.backgroundColor"
+      :color="option.color"
+      :label="option.label"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { SelectValue } from '@/interface/interface'
+import TagLabel from '@/components/TagLabel/TagLabel.vue'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(
+  defineProps<{
+    options: SelectValue[]
+  }>(),
+  {
+    options: () => [],
+  },
+)
+</script>
