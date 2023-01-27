f90792b265c4f159636bbcf69d633f224502341b
Romain Boissieres <romain.boissieres@makina-corpus.com>
2023-01-27 10:08:01 +0100

    Github#198 - Move transform Addworkspace button and add intro

--- packages/nuxt-locokit/src/pages/workspace/index.vue
+++ packages/nuxt-locokit/src/pages/workspace/index.vue
@@ -4,78 +4,44 @@
       <h1 class="text-primary font-medium">
         {{ $t('pages.workspace.title') }}
       </h1>
-      <div class="mt-12">
-        <h2 class="text-primary">
-          {{ $t('pages.workspace.myWorkspace') }}
-        </h2>
-        <div
-          v-if="
-            workspacesStore.workspaces && workspacesStore.workspaces.length > 0
-          "
-          class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-8 flex-wrap shrink-0"
-        >
+      <p class="mt-4 ">{{ $t('pages.workspace.intro') }}
+      </p>
+      <NuxtLink :to="{ name: ROUTES_NAMES.WORKSPACE.CREATE }">
+        <PrimeButton class="!rounded-full !pr-5 !mt-4">
           <div
-            v-for="workspace in workspacesStore.workspaces"
-            :key="workspace"
+            class="relative flex flex-row justify-center text-center font-bold w-full">
+            <i class="bi bi-plus block font-medium" />
+            <p class="mx-autotext-primary pl-1">
+              {{ $t('pages.workspace.createWorkspace') }}
+            </p>
+          </div>
+        </PrimeButton>
+      </NuxtLink>
+      <div class="mt-12">
+        <div
+          v-if="workspacesStore.workspaces && workspacesStore.workspaces.length > 0"
+          class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-8 flex-wrap shrink-0">
+          <div v-for="workspace in workspacesStore.workspaces" :key="workspace"
             class="h-44 lg:h-56 bg-gray-200 text-black box-border rounded !border-dashed !border-2 !border-gray-300 hover:!border-primary"
             :style="{
               backgroundColor: workspace.settings?.backgroundColor,
               color: workspace.settings?.color,
-            }"
-          >
+            }">
             <div
-              class="relative overflow-hidden flex flex-col h-full justify-center text-center font-bold cursor-pointer"
-            >
+              class="relative overflow-hidden flex flex-col h-full justify-center text-center font-bold cursor-pointer">
               <NuxtLink class="text-theme-text">
                 <p class="text-2xl line-clamp-3">
                   {{ workspace.name }}
                 </p>
               </NuxtLink>
-              <i
-                class="absolute -left-3 -bottom-3 text-9xl opacity-10"
-                :class="`bi ${workspace.settings?.icon}`"
-              />
-              <span
-                v-if="workspace.public"
-                class="px-2 max-w-fit rounded absolute bottom-1 right-1 bg-gray-300 text-black text-sm"
-              >
+              <i class="absolute -left-3 -bottom-3 text-9xl opacity-10"
+                :class="`bi ${workspace.settings?.icon}`" />
+              <span v-if="workspace.public"
+                class="px-2 max-w-fit rounded absolute bottom-1 right-1 bg-gray-300 text-black text-sm">
                 {{ $t('pages.workspace.public') }}
               </span>
             </div>
           </div>
-          <NuxtLink :to="{ name: ROUTES_NAMES.WORKSPACE.CREATE }">
-            <PrimeButton
-              class="w-full h-44 lg:h-56 p-button-link box-border !border-dashed !border-2 !border-gray-300 rounded !p-0 hover:!border-primary"
-            >
-              <div
-                class="relative overflow-hidden flex flex-col justify-center text-center font-bold w-full"
-              >
-                <i class="bi bi-plus-circle-fill block !text-5xl" />
-                <p class="block mx-autotext-primary mt-4 text-xl">
-                  {{ $t('pages.workspace.newWorkspace') }}
-                </p>
-              </div>
-            </PrimeButton>
-          </NuxtLink>
-        </div>
-        <div
-          v-else
-          class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-8 flex-wrap shrink-0"
-        >
-          <NuxtLink :to="{ name: ROUTES_NAMES.WORKSPACE.CREATE }">
-            <PrimeButton
-              class="w-full h-44 lg:h-56 p-button-link box-border !border-dashed !border-2 !border-gray-300 rounded !p-0 hover:!border-primary"
-            >
-              <div
-                class="relative overflow-hidden flex flex-col justify-center text-center font-bold w-full"
-              >
-                <i class="bi bi-plus-circle-fill block !text-5xl" />
-                <p class="block mx-autotext-primary mt-4 text-xl">
-                  {{ $t('pages.workspace.createWorkspace') }}
-                </p>
-              </div>
-            </PrimeButton>
-          </NuxtLink>
         </div>
       </div>
     </div>
