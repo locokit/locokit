e4aeb1c9a4361f4abf3a3abc0789f2906f2092d7
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-01-30 10:14:11 +0100

    Enhance logger display

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -5,9 +5,30 @@ import type { HookContext, NextFunction } from './declarations'
 export const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
   level: 'debug',
-  format: format.combine(format.splat(), format.simple()),
+  format: format.combine(
+    format.colorize(),
+    format.ms(),
+    format.timestamp(),
+    format.printf(
+      ({
+        level,
+        message,
+        timestamp,
+        ms,
+        service,
+        ...args
+      }: Record<string, string>) => {
+        return `${timestamp} ${
+          service ? '[' + service + ']' : '[LocoKit]'
+        } ${level}: ${message} (${ms})`
+      },
+    ),
+  ),
   transports: [new transports.Console()],
+  exitOnError: false,
 })
+
+logger.debug('Logger created !')
 
 export const logErrorHook = async (
   _context: HookContext,
