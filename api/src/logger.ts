381c5e745e9684c6657e016a223dcec707b85e42
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-02-07 14:48:13 +0100

    [log] Enhance logger able to display error stacks, objects

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -1,28 +1,35 @@
 import { createLogger, format, transports } from 'winston'
 import type { HookContext, NextFunction } from './declarations'
+import util from 'node:util'
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 export const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
-  level: 'debug',
+  level: process.env.LOGGER_LEVEL,
   format: format.combine(
     format.colorize(),
     format.ms(),
     format.timestamp(),
-    format.printf(
-      ({
-        level,
-        message,
-        timestamp,
-        ms,
-        service,
-        ...args
-      }: Record<string, string>) => {
-        return `${timestamp} ${
-          service ? '[' + service + ']' : '[LocoKit]'
-        } ${level}: ${message} (${ms})`
-      },
-    ),
+    format.splat(),
+    format.errors({ stack: true }),
+    format.printf(({
+      level,
+      message,
+      timestamp,
+      ms,
+      service,
+      stack,
+      ...meta
+    }) => {
+      let messageToDisplay = `${timestamp} ${level} ${service ? '[' + service + ']' : '[LocoKit]'} : ${message} (${ms})`
+      if (stack) messageToDisplay += `\n${stack}`
+      // clean the meta from symbols
+      const metaWithoutSymbol = JSON.parse(JSON.stringify(meta))
+      if (Object.keys(metaWithoutSymbol).length > 0) {
+        messageToDisplay += `\n${util.format(metaWithoutSymbol)}`
+      }
+      return messageToDisplay
+    })
   ),
   transports: [new transports.Console()],
   exitOnError: false,
