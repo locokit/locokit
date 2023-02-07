f09a013ab08bd6a17ab617b34e9415b1bae75f56
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-02-07 14:47:39 +0100

    [test] Fix app tests

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import { describe, beforeAll, afterAll, it, expect } from 'vitest'
 import axios from 'axios'
 import { createApp } from './app'
 
@@ -16,25 +16,28 @@ describe('Feathers application tests', () => {
   })
 
   it('starts and shows the index page', async () => {
+    expect.assertions(1)
     const { data } = await axios.get<string>(appUrl)
 
-    assert.ok(data.includes('<html lang="en">'))
+    expect(data.includes('<html lang="en">')).toBeDefined()
   })
 
   it('shows a 404 JSON error', async () => {
+    expect.assertions(3)
     try {
       await axios.get(`${appUrl}/path/to/nowhere`, {
         responseType: 'json',
       })
-      assert.fail('should never get here')
+      expect.fail('should never get here')
     } catch (error: any) {
       const { response } = error
-      assert.strictEqual(response?.status, 404)
-      assert.strictEqual(response?.data?.code, 404)
-      assert.strictEqual(response?.data?.name, 'NotFound')
+      expect(response?.status).toStrictEqual(404)
+      expect(response?.data?.code).toStrictEqual(404)
+      expect(response?.data?.name).toStrictEqual('NotFound')
     }
   })
 
-  it("check that a workspace's schema role can't access the core schema")
-  it('check that a swagger is available for the api')
+  it.todo("check that a workspace's schema role can't access the core schema")
+  it.todo('check that a swagger is available for the api on swagger.json URL')
+  it.todo('check that the swagger displays itself on swagger.html URL')
 })
