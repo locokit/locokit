e099802eec19b5e1d2be4f20ada833506d082371
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-02-07 14:47:29 +0100

    Enhance user with firstname / lastname / blocked fields

--- api/migrations/20220705102228_user.ts
+++ api/migrations/20220705102228_user.ts
@@ -4,7 +4,8 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.withSchema('core').createTable('lck_user', (table) => {
     table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('gen_random_uuid()'))
-    table.string('name', 255)
+    table.string('firstname', 255)
+    table.string('lastname', 255)
     table.string('username', 255).unique().notNullable()
     table.string('avatarURL')
     table.string('email', 255).unique().notNullable()
@@ -12,6 +13,11 @@ export async function up(knex: Knex): Promise<void> {
     table
       .enum('profile', [USER_PROFILE.MEMBER, USER_PROFILE.CREATOR, USER_PROFILE.ADMIN])
       .defaultTo(USER_PROFILE.CREATOR)
+    table.boolean('blocked').defaultTo(false)
+
+    /**
+     * Fields for authentication management
+     */
     table.boolean('isVerified').defaultTo(false)
     table.string('verifyToken')
     table.string('verifyShortToken')
@@ -23,8 +29,8 @@ export async function up(knex: Knex): Promise<void> {
     table.integer('resetAttempts')
 
     table.datetime('createdAt').defaultTo(knex.fn.now()).notNullable()
-    table.datetime('lastConnection')
     table.datetime('updatedAt').defaultTo(knex.fn.now()).notNullable()
+    table.datetime('lastConnection')
   })
 }
 
