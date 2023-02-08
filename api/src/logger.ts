b81e3a617461586d513b796da1bcc75b9156ed30
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-02-08 18:57:46 +0100

    [log] Enhancement for logger

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -12,24 +12,26 @@ export const logger = createLogger({
     format.timestamp(),
     format.splat(),
     format.errors({ stack: true }),
-    format.printf(({
-      level,
-      message,
-      timestamp,
-      ms,
-      service,
-      stack,
-      ...meta
-    }) => {
-      let messageToDisplay = `${timestamp} ${level} ${service ? '[' + service + ']' : '[LocoKit]'} : ${message} (${ms})`
-      if (stack) messageToDisplay += `\n${stack}`
+    format.printf(({ level, message, timestamp, ms, service, stack, ...meta }) => {
+      let messageToDisplay = `${timestamp as string} ${level} ${
+        service ? '[' + (service as string) + ']' : '[LocoKit]'
+      } : ${message as string} (${ms as string})`
+      if (stack) messageToDisplay += `\n${stack as string}`
       // clean the meta from symbols
-      const metaWithoutSymbol = JSON.parse(JSON.stringify(meta))
+      const metaWithoutSymbol = Object.entries(meta).reduce((acc: Record<string, any>, [k, v]) => {
+        /**
+         * The key 'hook' is not formatable via util.format later...
+         */
+        if (k === 'hook') return acc
+        if (typeof k !== 'symbol') acc[k as string] = v
+        return acc
+      }, {})
+
       if (Object.keys(metaWithoutSymbol).length > 0) {
         messageToDisplay += `\n${util.format(metaWithoutSymbol)}`
       }
       return messageToDisplay
-    })
+    }),
   ),
   transports: [new transports.Console()],
   exitOnError: false,
@@ -37,10 +39,10 @@ export const logger = createLogger({
 
 logger.debug('Logger created !')
 
-export const logErrorHook = async (
-  _context: HookContext,
-  next: NextFunction,
-): Promise<void> => {
+/**
+ * Around hook used to manage the errors at the app level
+ */
+export const logErrorHook = async (_context: HookContext, next: NextFunction): Promise<void> => {
   try {
     await next()
   } catch (error) {
