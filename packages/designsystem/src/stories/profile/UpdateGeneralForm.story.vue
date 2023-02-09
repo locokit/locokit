d2e335f5881962806678b907a64e18bf8113d6eb
Aline Chetta <aline.chetta@makina-corpus.com>
2023-02-09 14:48:26 +0100

    Retrieve story content

---
+++ packages/designsystem/src/stories/profile/UpdateGeneralForm.story.vue
@@ -0,0 +1,26 @@
+<template>
+  <Story title="Profile/UpdateUsernameForm Form" icon="bi:person-fill">
+    <Variant title="default">
+      <UpdateGeneralForm
+        :user="{
+          id: 1,
+          email: 'contact@locokit.com',
+          profile: 'ADMIN',
+          name: 'Companion Cube',
+        }"
+        @submit="logEvent('submit', $event)"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import { logEvent } from 'histoire/client'
+import UpdateGeneralForm from '../../components/profile/UpdateGeneralForm/UpdateGeneralForm.vue'
+</script>
+
+<docs lang="md">
+### UpdateGeneralForm
+
+Form which allow an user to update its profile if this user is already logged.
+</docs>
