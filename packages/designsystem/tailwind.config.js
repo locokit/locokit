c5418ccc805e07b5a1875bd734c2618c4aac7817
Aline Chetta <aline.chetta@makina-corpus.com>
2023-03-06 12:10:39 +0100

    Update package in design system

--- packages/designsystem/tailwind.config.js
+++ packages/designsystem/tailwind.config.js
@@ -1,22 +1,34 @@
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--color-primary)',
-        'primary-lighten': 'var(--color-primary-lighten)',
-        'primary-light': 'var(--color-primary-light)',
-        'primary-dark': 'var(--color-primary-dark)',
-        secondary: 'var(--color-secondary)',
-        'secondary-lighten': 'var(--color-secondary-lighten)',
-        'secondary-light': 'var(--color-secondary-light)',
-        'secondary-dark': 'var(--color-secondary-dark)',
-        error: colors.red['500'],
+        primary: 'var(--primary-color)',
+        'primary-lighten': 'var(--primary-color-lighten)',
+        'primary-light': 'var--primary-color-light)',
+        'primary-dark': 'var(--primary-color-dark)',
+        secondary: 'var(--secondary-color)',
+        'secondary-lighten': 'var(--secondary-color-lighten)',
+        'secondary-light': 'var(--secondary-color-light)',
+        'secondary-dark': 'var(--secondary-color-dark)',
+        error: 'var(--color-error)',
+        'error-lighten': 'var(--color-error-lighten)',
+        'error-light': 'var(--color-error-light)',
+        'error-dark': 'var(--color-error-dark)',
+        success: 'var(--color-success)',
+        'success-lighten': 'var(--color-success-lighten)',
+        'success-light': 'var(--color-success-light)',
+        'success-dark': 'var(--color-success-dark)',
+        warning: 'var(--color-warning)',
+        'warning-lighten': 'var(--color-warning-lighten)',
+        'warning-light': 'var(--color-warning-light)',
+        'warning-dark': 'var(--color-warning-dark)',
+      },
+      borderRadius: {
+        lck: 'var(--border-radius)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
