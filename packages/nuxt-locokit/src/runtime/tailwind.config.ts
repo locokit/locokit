c115e296e954e3074d20888e0f55d19766bcef2f
Aline Chetta <aline.chetta@makina-corpus.com>
2023-03-07 16:31:55 +0100

    Remove explicit screen value

--- packages/nuxt-locokit/src/runtime/tailwind.config.ts
+++ packages/nuxt-locokit/src/runtime/tailwind.config.ts
@@ -28,18 +28,6 @@ export default {
           text: 'var(--theme-text-color)',
         },
       },
-      screens: {
-        sm: '640px',
-        // => @media (min-width: 640px) { ... }
-        md: '768px',
-        // => @media (min-width: 768px) { ... }
-        lg: '1024px',
-        // => @media (min-width: 1024px) { ... }
-        xl: '1280px',
-        // => @media (min-width: 1280px) { ... }
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      },
     },
   },
   plugins: [require('@tailwindcss/line-clamp')],
