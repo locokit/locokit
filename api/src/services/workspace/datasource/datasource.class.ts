683efce34669e97b88fcb3224232cc2c0dd1a679
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-03-08 23:10:18 +0100

    [api/datasource] Move sync hook to sync class function

--- api/src/services/workspace/datasource/datasource.class.ts
+++ api/src/services/workspace/datasource/datasource.class.ts
@@ -11,32 +11,36 @@ import type { DatasourceData, DatasourceResult, DatasourceQuery } from './dataso
 import { TableResult } from '../table/table.schema'
 // import { TableField } from '../table-field/table-field.class'
 import { TableFieldResult } from '../table-field/table-field.schema'
-import { DB_TYPE } from '@locokit/definitions/src/fieldType'
+import { convertDBTypeToFieldType, DB_TYPE } from '@locokit/definitions/src/fieldType'
+import { Application } from '@/declarations'
+import { SERVICES } from '@locokit/definitions'
 
 const datasourceLogger = logger.child({ service: 'datasource' })
 
-export interface DatasourceParams extends KnexAdapterParams<DatasourceQuery> { }
+export interface DatasourceParams extends KnexAdapterParams<DatasourceQuery> {}
 
 export interface Migration {
   action: 'CREATE' | 'UPDATE' | 'REMOVE'
   target: 'TABLE' | 'FIELD' | 'RELATION'
   direction: 'METAMODEL' | 'DATASOURCE'
-  settings: Partial<KnexInspectorTable> & Partial<KnexInspectorColumn> & Partial<{
-    dbType: DB_TYPE
-    documentation: string | null
-    type: string | null
-    nullable: boolean
-    primary: boolean
-    default: string | number | null
-    foreign: boolean
-    maxLength: number | null
-    fromTable: string
-    fromSchema: string | null
-    fromField: string
-    toTable: string | null
-    toField: string
-    toSchema: string | null
-  }>
+  settings: Partial<KnexInspectorTable> &
+    Partial<KnexInspectorColumn> &
+    Partial<{
+      dbType: DB_TYPE
+      documentation: string | null
+      type: string | null
+      nullable: boolean
+      primary: boolean
+      default: string | number | null
+      foreign: boolean
+      maxLength: number | null
+      fromTable: string
+      fromSchema: string | null
+      fromField: string
+      toTable: string | null
+      toField: string
+      toSchema: string | null
+    }>
 }
 
 export class Datasource extends ObjectionService<
@@ -44,21 +48,29 @@ export class Datasource extends ObjectionService<
   DatasourceData,
   DatasourceParams
 > {
+  app!: Application
+
+  async setup(app: Application) {
+    this.app = app
+  }
   /**
    * This custom method will analyze diffs
    * between real schema and meta model stored in LocoKit
    *
    * Real schema > Meta model
    */
-  async diff(data: { id: string }, params: DatasourceParams): Promise<{
-    datasource: DatasourceResult,
+  async diff(
+    data: { id: string },
+    params: DatasourceParams,
+  ): Promise<{
+    datasource: DatasourceResult
     diff: Migration[]
   }> {
     datasourceLogger.debug('computing diff between remote schema and meta model')
 
     const { authentication, provider, transaction, authenticated, user, route } = params
 
-    const datasource = await this.get(data.id, {
+    const datasource = (await this.get(data.id, {
       authentication,
       provider,
       transaction,
@@ -68,7 +80,7 @@ export class Datasource extends ObjectionService<
       query: {
         $eager: '[tables.[fields]]', // relations
       },
-    }) as DatasourceResult
+    })) as DatasourceResult
 
     const dsParams = {
       type: datasource.client,
@@ -101,13 +113,13 @@ export class Datasource extends ObjectionService<
             name: table.name,
             schema: table.schema,
             documentation: table.comment,
-          }
+          },
         })
 
         /**
          * Add all fields to the diff to apply
          */
-        table.fields.forEach(field => {
+        table.fields.forEach((field) => {
           diffToApply.push({
             action: 'CREATE',
             target: 'FIELD',
@@ -122,8 +134,8 @@ export class Datasource extends ObjectionService<
               primary: field.is_primary_key,
               default: field.default_value,
               foreign: field.foreign_key_column ? true : false,
-              maxLength: field.max_length
-            }
+              maxLength: field.max_length,
+            },
           })
 
           if (field.foreign_key_column) {
@@ -137,17 +149,15 @@ export class Datasource extends ObjectionService<
                 fromField: field.name,
                 toTable: field.foreign_key_table,
                 toField: field.foreign_key_column,
-                toSchema: field.foreign_key_schema
-              }
+                toSchema: field.foreign_key_schema,
+              },
             })
           }
         })
-
-      }
-      /**
-       * Else, find diffs at the table & fields level
-      */
-      else {
+      } else {
+        /**
+         * Else, find diffs at the table & fields level
+         */
         /**
          * Find diff for the current table ?
          */
@@ -159,13 +169,15 @@ export class Datasource extends ObjectionService<
             table: table.name,
             schema: table.schema,
             documentation: table.comment,
-          }
+          },
         })
         /**
          * For each field, create table-field if it does not already exist
          */
-        table.fields.forEach(field => {
-          const fieldFromMetaModel: TableFieldResult = tableFromMetaModel.fields?.find(f => f.slug === field.name)
+        table.fields.forEach((field) => {
+          const fieldFromMetaModel: TableFieldResult = tableFromMetaModel.fields?.find(
+            (f) => f.slug === field.name,
+          )
           if (!fieldFromMetaModel) {
             diffToApply.push({
               action: 'CREATE',
@@ -181,8 +193,8 @@ export class Datasource extends ObjectionService<
                 primary: field.is_primary_key,
                 default: field.default_value,
                 foreign: field.foreign_key_column ? true : false,
-                maxLength: field.max_length
-              }
+                maxLength: field.max_length,
+              },
             })
 
             if (field.foreign_key_column) {
@@ -196,8 +208,8 @@ export class Datasource extends ObjectionService<
                   fromField: field.name,
                   toTable: field.foreign_key_table,
                   toField: field.foreign_key_column,
-                  toSchema: field.foreign_key_schema
-                }
+                  toSchema: field.foreign_key_schema,
+                },
               })
             }
           } else {
@@ -218,8 +230,8 @@ export class Datasource extends ObjectionService<
                 primary: field.is_primary_key,
                 default: field.default_value,
                 foreign: field.foreign_key_column ? true : false,
-                maxLength: field.max_length
-              }
+                maxLength: field.max_length,
+              },
             })
 
             if (field.foreign_key_column) {
@@ -233,13 +245,12 @@ export class Datasource extends ObjectionService<
                   fromField: field.name,
                   toTable: field.foreign_key_table,
                   toField: field.foreign_key_column,
-                  toSchema: field.foreign_key_schema
-                }
+                  toSchema: field.foreign_key_schema,
+                },
               })
             }
           }
         })
-
       }
     })
 
@@ -250,7 +261,7 @@ export class Datasource extends ObjectionService<
 
     return {
       datasource,
-      diff: diffToApply
+      diff: diffToApply,
     }
   }
 
@@ -260,11 +271,154 @@ export class Datasource extends ObjectionService<
    *
    * Real schema > Meta model
    */
-  async sync(data: { id: string }, params: DatasourceParams): Promise<{
-    datasource: DatasourceResult,
+  async sync(
+    data: { id: string },
+    params: DatasourceParams,
+  ): Promise<{
+    datasource: DatasourceResult
     diff: Migration[]
   }> {
-    datasourceLogger.debug('syncing the datasource %s, no matter which diff there are', data.id, data)
-    return this.diff(data, params)
+    datasourceLogger.debug(
+      'syncing the datasource %s, no matter which diff there are',
+      data.id,
+      data,
+    )
+    const { datasource, diff } = await this.diff(data, params)
+
+    if (diff.length > 0) {
+      datasourceLogger.debug('applying migration for datasource %s', data.id)
+
+      /**
+       * Build the initial matching with existant tables and fields in the datasource
+       */
+      const matchingIds = new Map()
+      // const currentDatasource = await context.service.get(context.data?.id as Id, { query: { $eager: '[tables.[fields]]' } }) as DatasourceResult
+      datasource.tables?.forEach((t: TableResult) => {
+        matchingIds.set('table_' + t.name, t.id)
+        t.fields?.forEach((f: TableFieldResult) => {
+          matchingIds.set('field_' + t.name + '_' + f.name, f.id)
+        })
+      })
+
+      datasourceLogger.debug('current matching ids', matchingIds)
+
+      /**
+       * Creating tables
+       */
+      await Promise.all(
+        diff
+          .filter(
+            (m) => m.direction === 'METAMODEL' && m.target === 'TABLE' && m.action === 'CREATE',
+          )
+          .map(async (m) => {
+            datasourceLogger.debug('creating table %s', m.settings.name, {
+              name: m.settings.name,
+              slug: m.settings.name,
+              datasourceId: datasource.id,
+            })
+            const table = await this.app.service(SERVICES.WORKSPACE_TABLE).create(
+              {
+                name: m.settings.name as string,
+                slug: m.settings.name as string,
+                datasourceId: data!.id as string,
+              },
+              {
+                transaction: params.transaction,
+              },
+            )
+            matchingIds.set('table_' + m.settings.name, table.id)
+          }),
+      )
+
+      /**
+       * Creating fields
+       */
+      await Promise.all(
+        diff
+          .filter(
+            (m) => m.direction === 'METAMODEL' && m.target === 'FIELD' && m.action === 'CREATE',
+          )
+          .map(async (m) => {
+            const tableId = matchingIds.get('table_' + m.settings.table)
+            datasourceLogger.debug('creating field %s', m.settings.name, {
+              name: m.settings.name,
+              slug: m.settings.name,
+              type: convertDBTypeToFieldType(datasource.client, m.settings.dbType),
+              dbType: m.settings.dbType,
+              settings: m.settings,
+              tableId: matchingIds.get('table_' + m.settings.table),
+            })
+            if (!tableId)
+              throw new Error(
+                `Sync error, no table matching for field ${m.settings.table}.${m.settings.name}`,
+              )
+
+            const field = await this.app.service(SERVICES.WORKSPACE_TABLE_FIELD).create(
+              {
+                name: m.settings.name as string,
+                slug: m.settings.name as string,
+                type: convertDBTypeToFieldType(datasource.client, m.settings.dbType),
+                dbType: m.settings.dbType as DB_TYPE,
+                settings: m.settings,
+                tableId: matchingIds.get('table_' + m.settings.table),
+              },
+              {
+                transaction: params.transaction,
+              },
+            )
+            matchingIds.set('field_' + m.settings.table + '_' + m.settings.name, field.id)
+          }),
+      )
+
+      /**
+       * Creating relations
+       */
+      await Promise.all(
+        diff
+          .filter(
+            (m) => m.direction === 'METAMODEL' && m.target === 'RELATION' && m.action === 'CREATE',
+          )
+          .map(async (m) => {
+            const fromTableId = matchingIds.get('table_' + m.settings.fromTable)
+            const toTableId = matchingIds.get('table_' + m.settings.toTable)
+            const fromFieldId = matchingIds.get(
+              'field_' + m.settings.fromTable + '_' + m.settings.fromField,
+            )
+            const toFieldId = matchingIds.get(
+              'field_' + m.settings.toTable + '_' + m.settings.toField,
+            )
+            if (!fromTableId || !toTableId || !fromFieldId || !toFieldId)
+              throw new Error(
+                `Sync error, missing table/field matching for relation between ${m.settings.fromTable} and ${m.settings.toTable}`,
+              )
+
+            datasourceLogger.debug('creating relation %s', m.settings.name, {
+              fromTableId,
+              fromFieldId,
+              toTableId,
+              toFieldId,
+              type: '1-n',
+              settings: m.settings,
+            })
+
+            const relation = await this.app.service(SERVICES.WORKSPACE_TABLE_RELATION).create(
+              {
+                fromTableId,
+                fromFieldId,
+                toTableId,
+                toFieldId,
+                type: '1-n',
+                settings: m.settings,
+              },
+              {
+                transaction: params.transaction,
+              },
+            )
+            // matchingIds.set('relation_' + m.settings.fromTable + ', relation.id)
+          }),
+      )
+    }
+
+    return { datasource, diff }
   }
 }
--- api/src/services/workspace/datasource/datasource.hooks.ts
+++ api/src/services/workspace/datasource/datasource.hooks.ts
@@ -1,20 +1,12 @@
 import { authenticate } from '@feathersjs/authentication'
 import { hooks as schemaHooks } from '@feathersjs/schema'
-import { convertDBTypeToFieldType, SERVICES } from '@locokit/definitions'
-import { iff } from 'feathers-hooks-common'
 
 import { transaction } from '@/feathers-objection'
 import { HookContext } from '@/declarations'
 import { WorkspaceResult } from '@/services/core/workspace/core-workspace.schema'
-import { logger } from '@/logger'
 
 import { datasourceResolvers } from './datasource.resolver'
-import { datasourceDataValidator, datasourceQueryValidator, DatasourceResult } from './datasource.schema'
-import { Datasource, Migration } from './datasource.class'
-import { TableResult } from '../table/table.schema'
-import { TableFieldResult } from '../table-field/table-field.schema'
-
-const datasourceHooksLogger = logger.child({ service: 'datasource-hooks' })
+import { datasourceDataValidator, datasourceQueryValidator } from './datasource.schema'
 
 export const datasourceHooks = {
   around: {
@@ -58,127 +50,7 @@ export const datasourceHooks = {
     ],
   },
   after: {
-    all: [
-      iff(context => context.method !== 'sync', transaction.end())
-    ],
-    sync: [
-      async (context: HookContext<Datasource>) => {
-        datasourceHooksLogger.debug('datasource hook after %s', context.method)
-        const { datasource, diff } = context.result as { datasource: DatasourceResult, diff: Migration[] }
-        if (diff.length > 0) {
-          datasourceHooksLogger.debug('applying migration for datasource %s', context.data.id)
-
-          /**
-           * Build the initial matching with existant tables and fields in the datasource
-           */
-          const matchingIds = new Map()
-          // const currentDatasource = await context.service.get(context.data?.id as Id, { query: { $eager: '[tables.[fields]]' } }) as DatasourceResult
-          datasource.tables?.forEach((t: TableResult) => {
-            matchingIds.set('table_' + t.name, t.id)
-            t.fields?.forEach((f: TableFieldResult) => {
-              matchingIds.set('field_' + t.name + '_' + f.name, f.id)
-            })
-          })
-
-          datasourceHooksLogger.debug('current matching ids', matchingIds)
-
-          /**
-           * Creating tables
-           */
-          await Promise.all(diff
-            .filter(m => m.direction === 'METAMODEL' && m.target === 'TABLE' && m.action === 'CREATE')
-            .map(async m => {
-              datasourceHooksLogger.debug('creating table %s', m.settings.name, {
-                name: m.settings.name,
-                slug: m.settings.name,
-                datasourceId: context.id
-              })
-              const table = await context.app.service(SERVICES.WORKSPACE_TABLE)
-                .create({
-                  name: m.settings.name,
-                  slug: m.settings.name,
-                  datasourceId: context.data.id
-                }, {
-                  transaction: context.params.transaction,
-                })
-              matchingIds.set('table_' + m.settings.name, table.id)
-            })
-          )
-
-          /**
-           * Creating fields
-           */
-          await Promise.all(diff
-            .filter(m => m.direction === 'METAMODEL' && m.target === 'FIELD' && m.action === 'CREATE')
-            .map(async m => {
-
-              const tableId = matchingIds.get('table_' + m.settings.table)
-              datasourceHooksLogger.debug('creating field %s', m.settings.name, {
-                name: m.settings.name,
-                slug: m.settings.name,
-                type: convertDBTypeToFieldType(datasource.client, m.settings.dbType),
-                dbType: m.settings.dbType,
-                settings: m.settings,
-                tableId: matchingIds.get('table_' + m.settings.table)
-              })
-              if (!tableId) throw new Error(`Sync error, no table matching for field ${m.settings.table}.${m.settings.name}`)
-
-              const field = await context.app.service(SERVICES.WORKSPACE_TABLE_FIELD).create({
-                name: m.settings.name,
-                slug: m.settings.name,
-                type: convertDBTypeToFieldType(datasource.client, m.settings.dbType),
-                dbType: m.settings.dbType,
-                settings: m.settings,
-                tableId: matchingIds.get('table_' + m.settings.table)
-              }, {
-                transaction: context.params.transaction,
-              })
-              matchingIds.set('field_' + m.settings.table + '_' + m.settings.name, field.id)
-            })
-          )
-
-          /**
-           * Creating relations
-           */
-          await Promise.all(diff
-            .filter(m => m.direction === 'METAMODEL' && m.target === 'RELATION' && m.action === 'CREATE')
-            .map(async m => {
-              const fromTableId = matchingIds.get('table_' + m.settings.fromTable)
-              const toTableId = matchingIds.get('table_' + m.settings.toTable)
-              const fromFieldId = matchingIds.get('field_' + m.settings.fromTable + '_' + m.settings.fromField)
-              const toFieldId = matchingIds.get('field_' + m.settings.toTable + '_' + m.settings.toField)
-              if (!fromTableId || !toTableId || !fromFieldId || !toFieldId)
-                throw new Error(`Sync error, missing table/field matching for relation between ${m.settings.fromTable} and ${m.settings.toTable}`)
-
-              datasourceHooksLogger.debug('creating relation %s', m.settings.name, {
-                fromTableId,
-                fromFieldId,
-                toTableId,
-                toFieldId,
-                type: '1-n',
-                settings: m.settings,
-              })
-
-              const relation = await context.app.service(SERVICES.WORKSPACE_TABLE_RELATION).create({
-                fromTableId,
-                fromFieldId,
-                toTableId,
-                toFieldId,
-                type: '1-n',
-                settings: m.settings,
-              }, {
-                transaction: context.params.transaction,
-              })
-              // matchingIds.set('relation_' + m.settings.fromTable + ', relation.id)
-            })
-          )
-
-
-        }
-        return context
-      },
-      transaction.end()
-    ]
+    all: [transaction.end()],
   },
   error: {
     all: [transaction.rollback()],
