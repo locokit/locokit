464190db6ffe9e55be04669101713a25e570dd62
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-03-29 23:12:35 +0200

    [pkg/engine] Enhance sql type conversion

--- packages/engine/utils/sqlTypeConverter.ts
+++ packages/engine/utils/sqlTypeConverter.ts
@@ -3,15 +3,20 @@ export function getJSONTypeFromSQLType(sqlType: string) {
     case 'jsonb':
       return 'object'
     case 'integer':
+    case 'decimal':
       return 'number'
     case 'boolean':
+    case 'bool':
       return 'boolean'
     case 'uuid':
     case 'timestamp with time zone':
     case 'character varying':
     case 'text':
     case 'lck_snake_case':
-    default:
+    case 'varchar':
       return 'string'
+    default:
+      if (process.env.NODE_ENV === 'production') return 'string'
+      throw new Error(`Type ${sqlType} unknown, please find a conversion type first.`)
   }
 }
