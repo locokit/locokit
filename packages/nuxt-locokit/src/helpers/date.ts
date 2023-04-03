3ded0d08d2a9b1561084ac61455b53d5e8082617
Aline Chetta <aline.chetta@makina-corpus.com>
2023-04-03 17:26:43 +0200

    Remove RangeError from date (fct need to be tested)

--- packages/nuxt-locokit/src/helpers/date.ts
+++ packages/nuxt-locokit/src/helpers/date.ts
@@ -1,17 +1,6 @@
-import { formatISO, lightFormat, parseISO } from 'date-fns'
-import { TranslateResult } from 'vue-i18n'
+import { formatISO, parseISO } from 'date-fns'
 
-export function formatDateString(date: string, format: TranslateResult) {
-  try {
-    return lightFormat(parseISO(date), `${format}`)
-  } catch {
-    return 'no date'
-  }
-}
-
-export function formatDate(date: Date, format: TranslateResult) {
-  return lightFormat(date, `${format}`)
-}
+// Todo: To be tested with filters
 
 export function formatDateISO(date: Date) {
   return formatISO(date, { representation: 'date' })
@@ -26,6 +15,9 @@ export function getDateFromISOString(date: unknown) {
     if (typeof date === 'string') {
       return parseISO(date)
     }
-  } catch (RangeError) {}
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err)
+  }
   return null
 }
