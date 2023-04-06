ae0cc4f6288a80bfb2a1567b1a6d7bd3e0d2541a
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-04-06 15:03:19 +0200

    [api] Exclude configure file from tests

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
     build: { target: 'esnext' },
     plugins: [tsconfigPaths()],
+    test: {
+      exclude: [...configDefaults.exclude, 'src/configure.test.ts'],
+    },
   }
 })
