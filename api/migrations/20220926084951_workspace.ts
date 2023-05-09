fce5e5f0dc74da49f6fdb2a9fd3c8115232bcf67
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-05-09 23:21:04 +0200

    [migration] Update workspace name/slug limits + dropWorkspaceSchema

--- api/migrations/20220926084951_workspace.ts
+++ api/migrations/20220926084951_workspace.ts
@@ -6,8 +6,8 @@ export async function up(knex: Knex): Promise<void> {
 
     .createTable('lck_workspace', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('gen_random_uuid()'))
-      table.string('name').notNullable()
-      table.string('slug').unique()
+      table.string('name', 50).notNullable()
+      table.string('slug', 60).unique()
       table.index('slug', 'IDX_workspace_slug')
       table.boolean('legacy').defaultTo(false)
       table.boolean('public').defaultTo(false)
@@ -22,6 +22,7 @@ export async function up(knex: Knex): Promise<void> {
       table.uuid('createdBy').notNullable()
       table.foreign('createdBy', 'FK_workspace_user').references('id').inTable('core.lck_user')
       table.index('createdBy', 'IDX_workspace_createdBy')
+      table.datetime('softDeletedAt')
     })
 
     /**
@@ -80,7 +81,7 @@ export async function up(knex: Knex): Promise<void> {
      */
     .createTable('lck_datasource', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('gen_random_uuid()'))
-      table.string('name').notNullable()
+      table.string('name', 255).notNullable()
       table.string('slug')
       table.index('slug', 'IDX_datasource_slug')
       table.unique(['slug', 'workspaceId'], { indexName: 'IDX_UNQ_datasource_slug' })
@@ -116,7 +117,7 @@ export async function up(knex: Knex): Promise<void> {
      */
     .createTable('lck_table', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('gen_random_uuid()'))
-      table.string('name').notNullable()
+      table.string('name', 255).notNullable()
       table.string('slug')
       table.index('slug', 'IDX_table_slug')
       table.string('schema')
--- api/migrations/dropWorkspaceSchema.sql
+++ api/migrations/dropWorkspaceSchema.sql
@@ -2,17 +2,12 @@
 -- Function to drop a dedicated schema in LocoKit database
 -- for a specific workspace
 --
-CREATE OR REPLACE FUNCTION "core"."dropWorkspaceSchema" (IN "workspace_id" uuid)
-  RETURNS void
-  LANGUAGE 'plpgsql'
-  VOLATILE
-  PARALLEL UNSAFE
-  COST 100
-
+CREATE OR REPLACE FUNCTION core."dropWorkspaceSchema"(IN "workspace_slug" text)
+ RETURNS void
+ LANGUAGE plpgsql
 AS $BODY$
 
 DECLARE
-  v_workspace core.lck_workspace%ROWTYPE;
   v_schema varchar;
   v_role_readonly varchar;
   v_role_readwrite varchar;
@@ -23,16 +18,7 @@ BEGIN
 
   SET search_path = core;
 
-  -- SELECT the workspace
-  EXECUTE format('SELECT *
-  FROM lck_workspace
-  WHERE id = ''%s''', $1)
-  INTO v_workspace
-  USING workspace_id;
-
-  RAISE NOTICE 'Workspace found : slug = %, name = %', v_workspace.slug, v_workspace.name;
-
-  v_schema := 'w_' || v_workspace.slug;
+  v_schema := 'w_' || workspace_slug;
   v_role_readonly := v_schema || '_ro';
   v_role_readwrite := v_schema || '_rw';
 
@@ -42,18 +28,19 @@ BEGIN
   RAISE NOTICE 'Schema ''%'' dropped.', v_schema;
 
   -- DROP v_role_readwrite as it inherit from v_role_readonly
-  EXECUTE format('DROP ROLE %I', v_role_readwrite);
+  EXECUTE format('DROP ROLE IF EXISTS %I', v_role_readwrite);
   RAISE NOTICE 'Role ''%'' dropped.', v_role_readwrite;
 
   -- REVOKE access to core.user
   EXECUTE format('REVOKE ALL ON core.lck_user FROM %I', v_role_readonly);
   EXECUTE format('REVOKE USAGE ON SCHEMA core FROM %I', v_role_readonly);
 
-  EXECUTE format('DROP ROLE %I', v_role_readonly);
+  EXECUTE format('DROP ROLE IF EXISTS %I', v_role_readonly);
   RAISE NOTICE 'Role ''%'' dropped.', v_role_readonly;
 
 RAISE NOTICE 'End of drop of the workspace ''%''.', v_schema;
 
 
 END
-$BODY$;
+$BODY$
+;
