3d445f62ac2f23debe869097b97da40f98259b8a
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-05-10 16:29:40 +0200

    [quality] Add coverage lcov

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,23 +1,21 @@
 import { configDefaults, defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(() => {
-  return {
-    build: { target: 'esnext' },
-    plugins: [
-      tsconfigPaths({
-        root: './',
-      }),
-    ],
-    test: {
-      exclude: [...configDefaults.exclude, 'test/**', 'src/configure.test.ts'],
-      coverage: {
-        provider: 'c8',
-        reporter: ['text', 'json', 'html'],
-        enabled: true,
-        include: 'src/**',
-      },
-      singleThread: true,
+export default defineConfig({
+  build: { target: 'esnext' },
+  plugins: [
+    tsconfigPaths({
+      root: './',
+    }),
+  ],
+  test: {
+    exclude: [...configDefaults.exclude, 'test/**', 'src/configure.test.ts'],
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      enabled: true,
+      include: ['./src/**'],
     },
-  }
+    singleThread: true,
+  },
 })
