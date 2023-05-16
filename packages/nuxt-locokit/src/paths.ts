2827749b56659e356c3a9454503f4d12b12fed10
Aline Chetta <aline.chetta@makina-corpus.com>
2023-05-16 18:37:56 +0200

    Restore reminder router struture

--- packages/nuxt-locokit/src/paths.ts
+++ packages/nuxt-locokit/src/paths.ts
@@ -102,3 +102,60 @@ export const ROUTES_PATH = {
     SETTINGS: '/w/:id/settings',
   },
 }
+
+/*
+  Only for reminder purpose.
+  Like say in https://github.com/locokit/locokit/pull/228#discussion_r1194904557, path are not accurate.
+*/
+
+// const reminder = {
+//   /**
+//    * Front office
+//    */
+//   APP: {
+//     HOME: '/app',
+//     GROUP: '/app/:group',
+//     GROUP_PAGES: '/app/:group/p',
+//     GROUP_PAGE: '/app/:group/p/:page',
+//     GROUP_PAGE_DETAIL: '/app/:group/p/:page/d',
+//   },
+//   /**
+//    * Back office
+//    */
+//   ADMIN: {
+//     HOME: '/admin', // settings + stats
+//     // SETTINGS: '/settings',
+//     APP: {
+//       HOME: '/app',
+//       GROUP: '/:group',
+//       GROUP_PAGES: '/p',
+//       GROUP_PAGE: '/:page',
+//       GROUP_PAGE_DETAIL: '/d',
+//     },
+//     DATA_PROVIDERS: {
+//       HOME: '/dps',
+//       SETTINGS: '/:dp/settings',
+//       SCHEMA: '/:dp/schema',
+//       TABLES: '/:dp/t',
+//       TABLE: '/:dp/t/:table',
+//       TABLE_VIEWS: '/:dp/t/:table/v',
+//       TABLE_VIEW: '/:dp/t/:table/v/:view',
+//     },
+//     AUTOMATIONS: {
+//       HOME: '/automations',
+//     },
+//     MEDIAS: '/media',
+//     PERMISSIONS: {
+//       HOME: '/permissions',
+//       ACL: '/permissions/:acl',
+//     },
+//     USERS: {
+//       HOME: '/users',
+//       USER: '/users/:id',
+//     },
+//     GROUPS: {
+//       HOME: '/groups',
+//       GROUP: '/groups/:id',
+//     },
+//   },
+// }
