7759f5dcfaa591c6d1c456b5c2b52598e62fa7be
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-06-02 15:29:43 +0200

    [app] Add nuxt devtools

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,4 +3,8 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['nuxt-locokit'],
+
+  devtools: {
+    enabled: true,
+  },
 })
