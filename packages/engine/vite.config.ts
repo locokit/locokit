5adcbdf0de5182d31d36fa7de526b0af57142c68
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-07-27 17:20:08 +0200

    [pkg/engine] Make package buildable with vite

---
+++ packages/engine/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json'
+
+
+export default defineConfig(() => {
+  return {
+    build: {
+      sourcemap: true,
+      lib: {
+        name: '@locokit/engine',
+        entry: ['src/index.ts'],
+        formats: ['cjs', 'es', 'umd'],
+        fileName: (format: string, entry: string) => `${entry}.${format}.js`,
+      },
+      rollupOptions: {
+        external: Object.keys(pkg.dependencies),
+      },
+      target: 'node18',
+    },
+    plugins: [
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+  }
+})
