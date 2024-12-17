68f740f1c4c3b88449372ad302840f6cbc94637f
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2024-12-17 16:25:36 +0100

    Issue #307 - Adapt the code of the form component for updating user's email address to the new package

--- packages/vue-components/components/profile/update-email/update-email.stories.ts
+++ packages/vue-components/components/profile/update-email/update-email.stories.ts
@@ -1,38 +1,95 @@
-import { fn } from '@storybook/test';
-import UpdateEmailForm from './UpdateEmailForm.vue';
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryContext, StoryObj } from '@storybook/vue3'
+import { expect, fn, userEvent, waitFor, within } from '@storybook/test'
+import UpdateEmailForm from './update-email.vue'
 
 const meta: Meta<typeof UpdateEmailForm> = {
-  title: 'components/forms/UpdateEmailForm',
+  title: 'components/profile/update-email',
   component: UpdateEmailForm,
-  tags: ['autodocs'],
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onSubmit: fn() },
+  args: {
+    user: {
+      id: '3e5ffeeb-af13-412a-92e8-b3f3fad50593',
+      username: 'ralph',
+      lastName: '',
+      firstName: 'Ralph',
+      email: 'ralph@example.com',
+      profile: 'member',
+    },
+    // Use `fn` to spy on the submit event, which will appear in the actions panel
+    // once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+    onSubmit: fn(),
+  },
 }
+export default meta
 
-export default meta;
-type Story = StoryObj<typeof UpdateEmailForm>;
+type Story = StoryObj<typeof UpdateEmailForm>
 
 export const Default: Story = {
-  name: 'default one',
-  render: () => ({
-    components: { UpdateEmailForm },
-    template: `
-      <UpdateEmailForm
-        :user="{
-          id: 1,
-          email: 'contact@locokit.com',
-          profile: 'ADMIN',
-          name: 'Companion Cube',
-        }"
-      />
-    `
-  })
+  play: async ({ args, canvasElement }: StoryContext) => {
+    const canvas = within(canvasElement)
+
+    // Initial state test.
+    const message = canvas.queryByTestId('form-global-message')
+    const submitButton = canvas.getByRole('button', { name: "Submit" }) as HTMLButtonElement
+
+    await expect(message).not.toBeInTheDocument()
+    await expect(submitButton.disabled).toBe(false)
+
+    // Valid use case test.
+    const emailInput = canvas.getByRole('textbox', { name: /Email/i })
+    const passwordInput = canvas.getByLabelText(/Password/)
+
+    const user = userEvent.setup({ delay: 60 })
+    await user.type(emailInput, 'ralph.smith@example.com')
+    await user.type(passwordInput, 'password')
+    await user.click(submitButton)
+
+    await waitFor(() => expect(args.onSubmit).toHaveBeenCalledWith({
+      id: '3e5ffeeb-af13-412a-92e8-b3f3fad50593',
+      newEmail: 'ralph.smith@example.com',
+      password: 'password',
+    }))
+  },
 }
 
-      
-// <docs lang="md">
-// ### UpdateEmailForm
+export const Loading: Story = {
+  args: {
+    loading: true,
+  },
+  play: async ({ canvasElement }: StoryContext) => {
+    const canvas = within(canvasElement)
+    const submitButton = canvas.getByRole('button', { name: "Submit" }) as HTMLButtonElement
+    await expect(submitButton.disabled).toBe(true)
+  },
+}
 
-// Allow user to update its email
-// </docs>
+export const WithValidationError: Story = {
+  play: async ({ args, canvasElement }: StoryContext) => {
+    const canvas = within(canvasElement)
+
+    const emailInput = canvas.getByRole('textbox', { name: /Email/i })
+    const passwordInput = canvas.getByLabelText(/Password/)
+    const submitButton = canvas.getByRole('button', { name: "Submit" }) as HTMLButtonElement
+
+    // Oups, this email address is not valid.
+    const user = userEvent.setup({ delay: 60 })
+    await user.type(emailInput, 'ralph.smith')
+    await user.type(passwordInput, 'password')
+    await user.click(submitButton)
+
+    await waitFor(() => expect(args.onSubmit).not.toHaveBeenCalled())
+  },
+}
+
+export const WithMessage: Story = {
+  args: {
+    message: {
+      status: 'error',
+      text: "An error occurred, please retry later.",
+    },
+  },
+  play: async ({ canvasElement }: StoryContext) => {
+    const canvas = within(canvasElement)
+    const errorMessage = canvas.queryByText(/An error occurred/)
+    await expect(errorMessage).toBeInTheDocument()
+  },
+}
