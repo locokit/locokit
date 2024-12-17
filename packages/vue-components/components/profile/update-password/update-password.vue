cdad97819f9f76748fac8d524a7e1dd54478810f
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2024-12-17 18:24:00 +0100

    Issue #307 - Adapt the code of the form component for updating user's password to the new package

--- packages/vue-components/components/profile/update-password/update-password.vue
+++ packages/vue-components/components/profile/update-password/update-password.vue
@@ -1,165 +1,96 @@
 <template>
-  <FormGeneric
-    :display-reset-button="false"
-    label-tk-button-submit="locokit.components.updatePasswordForm.submit"
-    :response="response"
+  <generic-form
+    :fields
+    :buttons="{
+      submit: true,
+      reset: false,
+      cancel: false,
+    }"
+    :labels="{
+      submit: t('locokit.components.updateGeneralForm.submit'),
+    }"
     :loading="loading"
-    class="w-3/4"
+    :message="message"
     @submit="onSubmit"
-  >
-    <Field
-      v-slot="{ field, errorMessage }"
-      v-model="currentPassword"
-      class="mb-4 flex flex-col gap-2"
-      name="updatePasswordForm.currentPassword"
-      rules="required"
-      as="div"
-    >
-      <label for="name" class="label-field-required">
-        {{ $t('locokit.components.updatePasswordForm.currentPassword') }}
-      </label>
-      <PrimePassword
-        v-bind="field"
-        v-model="currentPassword"
-        input-id="password"
-        :class="{ 'p-invalid': errorMessage }"
-        toggle-mask
-        :feedback="false"
-        spellcheck="false"
-        autocorrect="off"
-        autocapitalize="none"
-        required
-      />
-      <span
-        v-if="errorMessage"
-        class="p-text-error"
-        role="alert"
-        aria-live="assertive"
-      >
-        {{ errorMessage }}
-      </span>
-    </Field>
-    <Field
-      v-slot="{ field, errorMessage }"
-      class="mb-4 flex flex-col gap-2"
-      name="updatePasswordForm.newPassword"
-      :rules="{ required: true, regex: `${regexPasswordRules}(?=.{8,})` }"
-      as="div"
-    >
-      <label for="name" class="label-field-required">
-        {{ $t('locokit.components.updatePasswordForm.newPassword') }}
-      </label>
-      <PrimePassword
-        v-model="newPassword"
-        input-id="newPassword"
-        :class="{ 'p-invalid': errorMessage }"
-        v-bind="field"
-        :feedback="true"
-        toggle-mask
-        :medium-regex="`${regexPasswordRules}(?=.{8,})`"
-        :strong-regex="`${regexPasswordRules}(?=.{12,})`"
-        :weak-label="
-          $t('locokit.components.updatePasswordForm.passwordStrength.weak')
-        "
-        :medium-label="
-          $t('locokit.components.updatePasswordForm.passwordStrength.medium')
-        "
-        :strong-label="
-          $t('locokit.components.updatePasswordForm.passwordStrength.strong')
-        "
-        :prompt-label="$t('locokit.components.updatePasswordForm.prompt')"
-        required
-        aria-describedby="password-rules"
-        autocomplete="new-password"
-        spellcheck="false"
-        autocorrect="off"
-        autocapitalize="none"
-        :pattern="regexPasswordRules"
-      />
-      <small id="password-rules" class="italic">
-        {{ $t('locokit.components.updatePasswordForm.rules') }}
-      </small>
-      <span
-        v-if="errorMessage"
-        class="p-text-error block"
-        role="alert"
-        aria-live="assertive"
-      >
-        {{ errorMessage }}
-      </span>
-    </Field>
-    <Field
-      v-slot="{ field, errorMessage }"
-      class="mb-4 flex flex-col gap-2"
-      name="updatePasswordForm.confirmPassword"
-      rules="required|confirmed:updatePasswordForm.newPassword"
-      as="div"
-    >
-      <label for="name" class="label-field-required">
-        {{ $t('locokit.components.updatePasswordForm.confirmPassword') }}
-      </label>
-      <PrimePassword
-        v-model="confirmPassword"
-        input-id="confirmPassword"
-        :class="{ 'p-invalid': errorMessage }"
-        v-bind="field"
-        :feedback="false"
-        toggle-mask
-        required
-        spellcheck="false"
-        autocorrect="off"
-        autocapitalize="none"
-      />
-      <span
-        v-if="errorMessage"
-        class="p-text-error"
-        role="alert"
-        aria-live="assertive"
-      >
-        {{ errorMessage }}
-      </span>
-    </Field>
-  </FormGeneric>
+  />
 </template>
 
 <script setup lang="ts">
-import PrimePassword from 'primevue/password'
-import FormGeneric from '@/components/forms/FormGeneric.vue'
-import { Field } from 'vee-validate'
-import { ref } from 'vue'
-import { regexPasswordRules } from '@/helpers/regex'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { FIELD_COMPONENT, FIELD_TYPE, LocoKitFormField, LocoKitMessage } from '@locokit/definitions'
+//import { regexPasswordRules } from '@/helpers/regex'
+import GenericForm from '@/components/commons/generic-form/generic-form.vue'
 
-const emit = defineEmits<
+const { t } = useI18n();
+
+const emit = defineEmits<{
+  /**
+   * Emitted when the submit button has been clicked
+   * and the form has been successfully validated.
+   */
   (
     e: 'submit',
     form: {
       currentPassword: string
       newPassword: string
     },
-  ) => void
->()
+  ): void
+}>()
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const props = withDefaults(
+withDefaults(
   defineProps<{
+    /** Is the form loading? `true` to put it in loading state. */
     loading?: boolean
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    response?: Error | Record<string, any> | null
+    /** A message to display into the form, just above the buttons. */
+    message?: LocoKitMessage
   }>(),
   {
     loading: false,
-    response: null,
   },
 )
 
-const currentPassword = ref()
-const newPassword = ref()
-const confirmPassword = ref()
+const fields = computed<LocoKitFormField[]>(() => {
+  return [
+    {
+      id: 'currentPassword',
+      label: t('locokit.components.updatePasswordForm.currentPassword'),
+      type: FIELD_TYPE.TEXT,
+      component: FIELD_COMPONENT.INPUT_PASSWORD,
+      validationRules: {
+        required: true,
+        maxLength: 255,
+      },
+    },
+    {
+      id: 'newPassword',
+      label: t('locokit.components.updatePasswordForm.newPassword'),
+      type: FIELD_TYPE.TEXT,
+      component: FIELD_COMPONENT.INPUT_PASSWORD,
+      validationRules: {
+        required: true,
+        maxLength: 255,
+        // TODO: check that input matches a regex
+      },
+    },
+    {
+      id: 'confirmPassword',
+      label: t('locokit.components.updatePasswordForm.confirmPassword'),
+      type: FIELD_TYPE.TEXT,
+      component: FIELD_COMPONENT.INPUT_PASSWORD,
+      validationRules: {
+        required: true,
+        maxLength: 255,
+        // TODO: check that input is identical to the other field's value
+      },
+    },
+  ]
+})
 
-const onSubmit = () => {
+const onSubmit = (values: Record<string, unknown>) => {
   emit('submit', {
-    currentPassword: currentPassword.value,
-    newPassword: newPassword.value,
+    currentPassword: values.currentPassword as string,
+    newPassword: values.newPassword as string,
   })
 }
 </script>
