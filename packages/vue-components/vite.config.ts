234d3c39d722b7539bfd63645f004191eb3d55cd
Mathieu Dartigues <mathieu@dartic.fr>
2025-01-07 12:11:03 +0100

    feat: Add vue devtools for vue-components pkg

--- packages/vue-components/vite.config.ts
+++ packages/vue-components/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 import path from 'path'
 import dts from 'vite-plugin-dts'
 import { fileURLToPath, URL } from 'node:url'
@@ -12,6 +14,7 @@ export default defineConfig({
       // include: [path.resolve(__dirname, './lib.ts')],
       tsconfigPath: './tsconfig.app.json',
     }),
+    vueDevTools(),
   ],
   build: {
     lib: {
