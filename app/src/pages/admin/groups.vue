d593a73c62b61cf24857a49148c328f4e0d1f105
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2025-03-11 18:34:42 +0100

    Issue #343 - Enable to filter groups by name

--- app/src/pages/admin/groups.vue
+++ app/src/pages/admin/groups.vue
@@ -8,9 +8,12 @@
           :to="{ name: ROUTE_NAMES.ADMIN.GROUPS.CREATE }"
           fluid
         />
+        <PrimeIconField class="mt-4">
+          <PrimeInputIcon class="bi bi-search" />
+          <PrimeInputText v-model="search" :placeholder="t('locokit.pages.adminGroups.search')" />
+        </PrimeIconField>
       </div>
-      <!-- <FilterBuilder fields-definition=""/> -->
-      <ul>
+      <ul v-if="groupsStore.groups?.data">
         <li v-for="group in groupsStore.groups?.data" :key="group.id">
           <RouterLink
             :to="{
@@ -37,6 +40,12 @@
           </RouterLink>
         </li>
       </ul>
+      <p v-else-if="search">
+        {{ t('locokit.pages.adminGroups.emptyResult') }}
+      </p>
+      <p v-else>
+        {{ t('locokit.pages.adminGroups.emptyList') }}
+      </p>
     </div>
     <div class="w-full px-6 py-8">
       <RouterView/>
@@ -45,29 +54,31 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import Button from 'primevue/button'
+import PrimeIconField from 'primevue/iconfield'
+import PrimeInputIcon from 'primevue/inputicon'
+import PrimeInputText from 'primevue/inputtext'
 import Tag from 'primevue/tag'
-import { FilterBuilder } from '@locokit/vue-components'
-import { FIELD_TYPE } from '@locokit/definitions'
 import { useStoreGroups } from '@/stores/groups'
+import type { GroupResult } from '@locokit/sdk'
 import ROUTE_NAMES from '@/router/routes'
 
 const { t } = useI18n()
 const groupsStore = useStoreGroups()
-
-// const fieldsDefinition = [
-//   {
-//     slug: 'long-name',
-//     name: 'Field with super mega long name',
-//     type: FIELD_TYPE.STRING,
-//   },
-// ]
+const search = ref<string | null>(null)
 
 onMounted(() => {
   groupsStore.fetchGroups({
     $joinEager: 'policy',
+  })
+})
+
+watch(search, (search) => {
+  groupsStore.fetchGroups({
+    $joinEager: 'policy',
+    name: { $ilike: `%${search}%` },
   })
 })
 </script>
